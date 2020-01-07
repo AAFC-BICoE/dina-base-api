829ae0596a2f7f12b058d48648a0107797e48e71
Jonathan keyuk <jonathan.keyuk@canada.ca>
2020-01-07 16:19:04 -0500

    Support_18292_configure_project_to_deploy_to_github_packages
    https://redmine.biodiversity.agr.gc.ca/issues/18292
    
    Remove unused file

--- deployToGitHubRegistry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "<settings><servers><server>\
-<id>github</id>\
-<username>AAFC-BICoE</username>\
-<password>${GITHUB_TOKEN}</password>\
-</server></servers></settings>" > ~/.m2/settings.xml
-mvn deploy
