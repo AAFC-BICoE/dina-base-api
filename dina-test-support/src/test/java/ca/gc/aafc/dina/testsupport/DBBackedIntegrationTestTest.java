2d6687f56bd3ff7fe5d6886944f5033820a123f8
Jkeyuk <Jonkero2@gmail.com>
2020-04-21 11:19:43 -0400

    19134_add_delete_by_property
    Add test case for deleteByProperty for deleting multiple entities by a shared property value.

--- dina-test-support/src/test/java/ca/gc/aafc/dina/testsupport/DBBackedIntegrationTestTest.java
+++ dina-test-support/src/test/java/ca/gc/aafc/dina/testsupport/DBBackedIntegrationTestTest.java
@@ -3,6 +3,9 @@ package ca.gc.aafc.dina.testsupport;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
@@ -30,6 +33,19 @@ public class DBBackedIntegrationTestTest extends DBBackedIntegrationTest {
 
     deleteByProperty(ComplexObject.class, "id", comp.getId(), true);
     assertNull(find(ComplexObject.class, comp.getId()));
+  }
+
+  @Test
+  public void deleteByProperty_MultipleEntities_AllEntitiesDelete() {
+    List<ComplexObject> persistedEntities = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      ComplexObject comp = persistEntity();
+      persistedEntities.add(comp);
+    }
+
+    // Delete all entities with same name
+    deleteByProperty(ComplexObject.class, "name", persistedEntities.get(0).getName());
+    persistedEntities.forEach(pe -> assertNull(find(ComplexObject.class, pe.getId())));
   }
 
   private ComplexObject persistEntity() {
