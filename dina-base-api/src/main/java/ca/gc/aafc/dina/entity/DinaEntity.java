d4c843d47910f7d309962cba2e0fd38a5f62d2d0
Jonathan Keyuk <Jonkero2@gmail.com>
2020-04-28 11:58:56 -0400

    19183 Dina Entity interface (#29)
    - Add DinaEntity interface with a small use case in tests

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/entity/DinaEntity.java
@@ -0,0 +1,11 @@
+package ca.gc.aafc.dina.entity;
+
+/**
+ * Represents a DINA entity with an id field.
+ * The id is usually the Primary Key and auto-generated.
+ */
+public interface DinaEntity {
+
+    Long getId();
+
+}
