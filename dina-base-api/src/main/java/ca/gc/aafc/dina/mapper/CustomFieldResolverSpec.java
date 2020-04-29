61cc434ff4441cd7ce2502bf5800ed8b433617d7
Jkeyuk <Jonkero2@gmail.com>
2020-04-29 12:44:12 -0400

    19186_dina_mapper
    Move CustomFieldResolvers to seperate class

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/mapper/CustomFieldResolverSpec.java
@@ -0,0 +1,19 @@
+package ca.gc.aafc.dina.mapper;
+
+import java.util.function.Function;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+
+/**
+ * @param <E> Entity type
+ */
+@Builder
+@Getter
+public class CustomFieldResolverSpec<E> {
+  @NonNull
+  private String field;
+  @NonNull
+  private Function<E, Object> resolver;
+}
