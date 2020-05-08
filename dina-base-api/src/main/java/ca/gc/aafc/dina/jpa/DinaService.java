52642bf27c3b02fc51c194ae3e7f55dbf3af2fec
Jkeyuk <Jonkero2@gmail.com>
2020-05-08 09:05:02 -0400

    19251_Dina_Service
    DinaService method stubs added

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/DinaService.java
@@ -0,0 +1,42 @@
+package ca.gc.aafc.dina.jpa;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import ca.gc.aafc.dina.entity.DinaEntity;
+
+public class DinaService<E extends DinaEntity> {
+
+  public E create(E entity) {
+    return null;// TODO
+  }
+
+  public E update(E entity) {
+    return null;// TODO
+  }
+
+  public void delete(E entity) {
+    // TODO
+  }
+
+  public List<E> findAllWhere(E entity, Map<String, Object> where) {
+    return null;// TODO
+  }
+
+  public E findOne(UUID uuid) {
+    return null;// TODO
+  }
+
+  public E preCreate(E entity) {
+    return null;// TODO
+  }
+
+  public E preUpdate(E entity) {
+    return null;// TODO
+  }
+
+  public void preDelete(E entity) {
+    // TODO
+  }
+}
