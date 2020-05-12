41aca54d02662bebaf9a89ef4a22f7a7f692b4fb
Jkeyuk <Jonkero2@gmail.com>
2020-05-12 12:12:42 -0400

    19251_Dina_Service
    java docs added

--- dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/DinaService.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/DinaService.java
@@ -16,6 +16,11 @@ import ca.gc.aafc.dina.entity.DinaEntity;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Service class for database interactions with a {@link DinaEntity}.
+ *
+ * @param <E> - Type of {@link DinaEntity}
+ */
 @Component
 @RequiredArgsConstructor(onConstructor_ = @Inject)
 public abstract class DinaService<E extends DinaEntity> {
@@ -23,22 +28,54 @@ public abstract class DinaService<E extends DinaEntity> {
   @NonNull
   private final BaseDAO baseDAO;
 
+  /**
+   * Persist an instance of the provided entity in the database.
+   *
+   * @param entity entity to persist
+   * @return returns the original entity.
+   */
   public E create(E entity) {
     preCreate(entity);
     baseDAO.save(entity);
     return entity;
   }
 
+  /**
+   * Merge the state of a given entity into the current persistence context.
+   *
+   * @param entity entity to update
+   * @return returns the managed instance the state was merged to.
+   */
   public E update(E entity) {
     preUpdate(entity);
     return baseDAO.createWithEntityManager(em -> em.merge(entity));
   }
 
+  /**
+   * Remove the given entity from the database.
+   *
+   * @param entity entity to delete
+   */
   public void delete(E entity) {
     preDelete(entity);
     baseDAO.delete(entity);
   }
 
+  /**
+   * <p>
+   * Returns a list of Entities of a given class where the fields match a given
+   * map.
+   * <p>
+   *
+   * <p>
+   * Given where map maps field names to values. Where map can be empty to find
+   * all. Where map can search on null values.
+   * <p>
+   * 
+   * @param entityClass - entity class to search on
+   * @param where       - map of fieldName::Values to match on
+   * @return list of Entities
+   */
   public List<E> findAllWhere(Class<E> entityClass, Map<String, Object> where) {
     Objects.requireNonNull(entityClass);
     Objects.requireNonNull(where);
@@ -64,14 +101,39 @@ public abstract class DinaService<E extends DinaEntity> {
     });
   }
 
+  /**
+   * Find an entity by it's NaturalId. The method assumes that the naturalId is
+   * unique.
+   * 
+   * @param naturalId   - id of entity
+   * @param entityClass - class of entity
+   * @return the matched entity
+   */
   public E findOne(Object naturalId, Class<E> entityClass) {
     return baseDAO.findOneByNaturalId(naturalId, entityClass);
   }
 
+  /**
+   * Run before the {@link DinaService#create()} method.
+   *
+   * @param entity entity being created by {@link DinaService#create()}
+   * @return returns the created entity.
+   */
   protected abstract E preCreate(E entity);
 
+  /**
+   * Run before the {@link DinaService#update()} method.
+   *
+   * @param entity entity being updated by {@link DinaService#update()}
+   * @return returns the updated entity.
+   */
   protected abstract E preUpdate(E entity);
 
+  /**
+   * Run before the {@link DinaService#delete()} method.
+   *
+   * @param entity entity being deleted by {@link DinaService#delete()}
+   */
   protected abstract void preDelete(E entity);
 
 }
