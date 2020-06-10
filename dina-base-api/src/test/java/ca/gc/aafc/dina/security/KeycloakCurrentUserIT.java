def5325a8f214d48b73bd7f34cd2bdbb6a2a2474
Jkeyuk <Jonkero2@gmail.com>
2020-06-10 09:13:42 -0400

    Feature_19419_Add_groups_and_roles_to_DinaAuthenticatedUser
    Update current user test for groups

--- dina-base-api/src/test/java/ca/gc/aafc/dina/security/KeycloakCurrentUserIT.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/security/KeycloakCurrentUserIT.java
@@ -1,9 +1,14 @@
 package ca.gc.aafc.dina.security;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.junit.jupiter.api.Test;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.mockito.Answers;
@@ -24,6 +29,8 @@ public class KeycloakCurrentUserIT {
 
   @Test
   public void getCurrentUser_whenLoggedIn_currentUserGiven() {
+    List<String> expectedGroups = Arrays.asList("group 1", "group 2");
+
     KeycloakAuthenticationToken mockToken = Mockito.mock(
       KeycloakAuthenticationToken.class,
       Answers.RETURNS_DEEP_STUBS
@@ -38,11 +45,19 @@ public class KeycloakCurrentUserIT {
         .getOtherClaims()
         .get("agent-identifier"))
       .thenReturn("a2cef694-10f1-42ec-b403-e0f8ae9d2ae6");
+    Mockito.when(
+        mockToken.getAccount()
+          .getKeycloakSecurityContext()
+          .getToken()
+          .getOtherClaims()
+          .get("groups"))
+      .thenReturn(expectedGroups);
 
     SecurityContextHolder.getContext().setAuthentication(mockToken);
 
     assertEquals("test-user", currentUser.getUsername());
     assertEquals("a2cef694-10f1-42ec-b403-e0f8ae9d2ae6", currentUser.getAgentIdentifer().toString());
+    assertTrue(CollectionUtils.isEqualCollection(currentUser.getGroups(), expectedGroups));
   }
 
 }
