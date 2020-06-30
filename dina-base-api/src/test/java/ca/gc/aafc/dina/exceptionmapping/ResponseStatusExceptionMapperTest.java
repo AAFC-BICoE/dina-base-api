8455fa40c594613f13504bbfc2f86937b639c709
xuemeigan <xuemei.gan@canada.ca>
2020-06-30 15:32:29 -0400

    Support_19669_Get_file_exception_reason_is_not_set_in_the_error_message_detail Remove the TestFileController and message settings.

--- dina-base-api/src/test/java/ca/gc/aafc/dina/exceptionmapping/ResponseStatusExceptionMapperTest.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/exceptionmapping/ResponseStatusExceptionMapperTest.java
@@ -1,14 +1,14 @@
 package ca.gc.aafc.dina.exceptionmapping;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.server.ResponseStatusException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -16,19 +16,14 @@ public class ResponseStatusExceptionMapperTest {
  
   @Inject
   private ResponseStatusExceptionMapper responseStatusExceptionMapper;
-  
-  @Inject
-  private TestFileController ftc;
 
-  private static final String bucketUnderTest = "testBucket";
-  private static final String fileUnderTest = "9ada0de3-b190-44d8-992d-f4d532bc11fb";
+  private static final String errorMessage = "Minio file or bucket not found";
 
   @Test
   public void testMapperCreatesReadableErrorMessages() throws Exception {
-    assertThrows(ResponseStatusException.class, ()-> ftc.downloadFile(bucketUnderTest, fileUnderTest));
     try {
-      ftc.downloadFile(bucketUnderTest, fileUnderTest);    
-    } catch (ResponseStatusException e) {
+      throw  new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage, null);  
+    } catch (ResponseStatusException e) {   
       assertEquals(e.getStatus().value(), responseStatusExceptionMapper.toErrorResponse(e).getHttpStatus());
       assertEquals(e.getMessage(), 
           responseStatusExceptionMapper.toErrorResponse(e).getErrors().stream().findFirst().get().getDetail());
