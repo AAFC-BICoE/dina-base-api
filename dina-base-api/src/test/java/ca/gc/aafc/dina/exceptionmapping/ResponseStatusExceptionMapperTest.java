36927e9085ab45ef6cef20eb1e7deb2e056d9ec3
Christian Gendreau <christiangendreau@gmail.com>
2020-07-02 15:07:40 -0400

    Support #19669 Get file exception reason is not set in the error message detail Change test to be a real unit test

--- dina-base-api/src/test/java/ca/gc/aafc/dina/exceptionmapping/ResponseStatusExceptionMapperTest.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/exceptionmapping/ResponseStatusExceptionMapperTest.java
@@ -1,32 +1,24 @@
 package ca.gc.aafc.dina.exceptionmapping;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import javax.inject.Inject;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.server.ResponseStatusException;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 public class ResponseStatusExceptionMapperTest {  
  
-  @Inject
-  private ResponseStatusExceptionMapper responseStatusExceptionMapper;
+  private ResponseStatusExceptionMapper responseStatusExceptionMapper = new ResponseStatusExceptionMapper();
 
   private static final String errorMessage = "Minio file or bucket not found";
 
   @Test
   public void testMapperCreatesReadableErrorMessages() throws Exception {
-    try {
-      throw  new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage, null);  
-    } catch (ResponseStatusException e) {   
-      assertEquals(e.getStatus().value(), responseStatusExceptionMapper.toErrorResponse(e).getHttpStatus());
-      assertEquals(e.getMessage(), 
-          responseStatusExceptionMapper.toErrorResponse(e).getErrors().stream().findFirst().get().getDetail());
-    }    
+
+    ResponseStatusException rsEx = new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage, null);  
+
+    assertEquals(rsEx.getStatus().value(), responseStatusExceptionMapper.toErrorResponse(rsEx).getHttpStatus());
+    assertEquals(rsEx.getMessage(), 
+          responseStatusExceptionMapper.toErrorResponse(rsEx).getErrors().stream().findFirst().get().getDetail());
+   
   }  
 }
