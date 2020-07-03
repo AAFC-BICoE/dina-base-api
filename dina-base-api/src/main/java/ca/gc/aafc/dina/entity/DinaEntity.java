fc93fae891fc22cfd374da307c24a7db4b713620
Christian Gendreau <christiangendreau@gmail.com>
2020-07-03 10:40:52 -0400

    Support #19663 Add group to DinaEntity group added to the interface with default value (null)

--- dina-base-api/src/main/java/ca/gc/aafc/dina/entity/DinaEntity.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/entity/DinaEntity.java
@@ -1,11 +1,21 @@
 package ca.gc.aafc.dina.entity;
 
 /**
- * Represents a DINA entity with an id field.
- * The id is usually the Primary Key and auto-generated.
+ * Represents a DINA entity with an id field. The id is usually the Primary Key
+ * and auto-generated.
  */
 public interface DinaEntity {
 
-    Integer getId();
+  Integer getId();
+
+  /**
+   * The group represents the group owning the entity. group is optional and null
+   * is return if an entity doesn't support it.
+   * 
+   * @return the name of the group or null
+   */
+  default String getGroup() {
+    return null;
+  }
 
 }
