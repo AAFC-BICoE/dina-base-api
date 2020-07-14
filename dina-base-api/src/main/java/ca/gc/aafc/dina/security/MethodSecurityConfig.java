8034e673cf2f1adea7a7dec7934dea9158d0ebaf
Jkeyuk <Jonkero2@gmail.com>
2020-07-14 09:42:35 -0400

    19662_permissions_restrictions
    MethodSecurityConfig sets the application context

--- dina-base-api/src/main/java/ca/gc/aafc/dina/security/MethodSecurityConfig.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/security/MethodSecurityConfig.java
@@ -1,6 +1,8 @@
 package ca.gc.aafc.dina.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
@@ -12,10 +14,14 @@ import org.springframework.security.config.annotation.method.configuration.Globa
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
 
+  @Autowired
+  private ApplicationContext context;
+
   @Override
   protected MethodSecurityExpressionHandler createExpressionHandler() {
     DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
     handler.setPermissionEvaluator(new GroupBasedPermissionEvaluator());
+    handler.setApplicationContext(context);
     return handler;
   }
 
