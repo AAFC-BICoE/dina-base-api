aaac74df9332d8356e3a25c61e49fd63d1fe55fb
Jkeyuk <Jonkero2@gmail.com>
2020-07-16 09:35:34 -0400

    19662_permissions_restrictions
    Creat authorization service

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/AuthorizationService.java
@@ -0,0 +1,33 @@
+package ca.gc.aafc.dina.service;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
+import ca.gc.aafc.dina.security.GroupBasedPermissionEvaluator;
+import ca.gc.aafc.dina.security.MethodSecurityConfig;
+
+/**
+ * Authorization service using spring security. Service is only available on
+ * keycloak.enabled = true. Use Optional injection when using this service.
+ */
+@Service
+@ConditionalOnProperty(value = "keycloak.enabled", havingValue = "true")
+public class AuthorizationService {
+
+  /**
+   * Proxy Method to invoke security authorization, Delegates to
+   * {@link GroupBasedPermissionEvaluator#hasDinaPermission(DinaAuthenticatedUser, Object)}.
+   * This method itself does nothing, spring proxies must be called from a
+   * seperate bean. @PreAuthorize is only enabled with keycloak, see
+   * {@link MethodSecurityConfig}. This method will not validate if keycloak is
+   * disabled.
+   *
+   * @param entity
+   */
+  @PreAuthorize("hasDinaPermission(@currentUser, #entity)")
+  public void authorizeByGroup(Object entity) {
+  }
+
+}
