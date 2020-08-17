9f718c4134e813c060da5d5e64d249fe48ef427c
Jkeyuk <Jonkero2@gmail.com>
2020-08-17 09:48:20 -0400

    20038 Add Role Based Permission
    Revert changes to DinaUserConfig as we are now mocking in the test case

--- dina-base-api/src/test/java/ca/gc/aafc/dina/DinaUserConfig.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/DinaUserConfig.java
@@ -1,30 +1,18 @@
 package ca.gc.aafc.dina;
 
-import java.util.Map;
-import java.util.Set;
-
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
-import ca.gc.aafc.dina.security.DinaRole;
 
 /** When you need an Authenticated user bean */
 @Configuration
 public class DinaUserConfig {
 
   public static final String AUTH_USER_NAME = "username";
-  public static final Map<String, Set<DinaRole>> ROLES_PER_GROUP = ImmutableMap.of("group 1",
-      ImmutableSet.of(DinaRole.COLLECTION_MANAGER));
 
   @Bean
   public DinaAuthenticatedUser user() {
-    return DinaAuthenticatedUser.builder()
-      .username(DinaUserConfig.AUTH_USER_NAME)
-      .rolesPerGroup(ROLES_PER_GROUP)
-      .build();
+    return DinaAuthenticatedUser.builder().username(DinaUserConfig.AUTH_USER_NAME).build();
   }
 }
