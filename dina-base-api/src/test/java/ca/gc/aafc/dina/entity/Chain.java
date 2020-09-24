6942282c5bf5ba03e25d713521354453cf161417
Jkeyuk <Jonkero2@gmail.com>
2020-09-24 12:54:25 -0400

    20400 dina repo eager load
    Add model

---
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/entity/Chain.java
@@ -0,0 +1,73 @@
+package ca.gc.aafc.dina.entity;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * The chain entity is an instance of a workflow definition (aka. Chain Template). It also contains
+ * meta data about the chain. A chain needs to have a chain template which describes the workflow
+ * being performed, and a group which has access to the workflow instance.
+ *
+ */
+@Entity
+@Table(name = "Chains")
+@Builder(toBuilder = true)
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@SuppressFBWarnings({ "EI_EXPOSE_REP", "EI_EXPOSE_REP2" })
+public class Chain implements DinaEntity {
+
+  @Getter(onMethod = @__({
+    @Id,
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    }))
+  private Integer id;
+
+  @Getter(onMethod = @__({
+    @NotNull,
+    @NaturalId
+    }))
+  private UUID uuid;
+
+  private String createdBy;
+
+  @Column(insertable = false, updatable = false)
+  private OffsetDateTime createdOn;
+
+  @Getter(onMethod = @__({
+    @Column(name = "groupname")
+  }))
+  private String group;
+
+  @NotNull
+  @Size(max = 50)
+  private String name;
+
+  @Getter(onMethod = @__({
+    @NotNull,
+    @ManyToOne(fetch = FetchType.LAZY),
+    @JoinColumn(name = "chaintemplateid")
+    }))
+  private ChainTemplate chainTemplate;
+
+}
