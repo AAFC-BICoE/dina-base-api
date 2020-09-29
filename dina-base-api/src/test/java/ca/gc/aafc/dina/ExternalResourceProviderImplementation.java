23a49b109d493006e61363c14f126e588c9d2e22
Jkeyuk <Jonkero2@gmail.com>
2020-09-29 10:25:17 -0400

    20389 support external relations
    make ExternalResourceProviderImplementation to be available for test comparison

--- dina-base-api/src/test/java/ca/gc/aafc/dina/ExternalResourceProviderImplementation.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/ExternalResourceProviderImplementation.java
@@ -9,7 +9,7 @@ import java.util.Map;
 @TestConfiguration
 public class ExternalResourceProviderImplementation implements ExternalResourceProvider {
 
-  private Map<String, String> map = ImmutableMap.of(
+  public final Map<String, String> map = ImmutableMap.of(
     "Person", "Person/api/v1/person",
     "Author", "Author/api/v1/author");
 
