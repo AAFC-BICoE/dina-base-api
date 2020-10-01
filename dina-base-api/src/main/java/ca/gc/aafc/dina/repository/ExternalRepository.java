c61a8e20162f141e10e7989ccbb74a1b05215b8d
Jkeyuk <Jonkero2@gmail.com>
2020-10-01 14:10:14 -0400

    20489 external relations in response
    external repo maps a dummy list

--- dina-base-api/src/main/java/ca/gc/aafc/dina/repository/ExternalRepository.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/repository/ExternalRepository.java
@@ -4,10 +4,13 @@ import ca.gc.aafc.dina.dto.ExternalRelationDto;
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ReadOnlyResourceRepositoryBase;
 import io.crnk.core.resource.annotations.JsonApiExposed;
+import io.crnk.core.resource.list.DefaultResourceList;
 import io.crnk.core.resource.list.ResourceList;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 @JsonApiExposed(false)
@@ -19,6 +22,10 @@ public class ExternalRepository extends ReadOnlyResourceRepositoryBase<ExternalR
 
   @Override
   public ResourceList<ExternalRelationDto> findAll(QuerySpec querySpec) {
-    return null;
+    List<ExternalRelationDto> dtos = querySpec.getFilters()
+      .stream()
+      .map(filterSpec -> ExternalRelationDto.builder().id(filterSpec.getValue()).build())
+      .collect(Collectors.toList());
+    return new DefaultResourceList<>(dtos, null, null);
   }
 }
