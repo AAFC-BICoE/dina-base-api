e844b46fedc45cef5185999ae1ca49c8d5d85acb
Jkeyuk <Jonkero2@gmail.com>
2020-10-07 12:03:35 -0400

    20489 external relations in response
    ExternalTypeRepoInvoker refactored for common logic

--- dina-base-api/src/main/java/ca/gc/aafc/dina/repository/ExternalTypeRepoInvoker.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/repository/ExternalTypeRepoInvoker.java
@@ -2,8 +2,8 @@ package ca.gc.aafc.dina.repository;
 
 import ca.gc.aafc.dina.dto.ExternalRelationDto;
 import ca.gc.aafc.dina.repository.meta.ExternalResourceProvider;
-import io.crnk.core.engine.information.InformationBuilder;
 import io.crnk.core.engine.information.resource.ResourceFieldType;
+import io.crnk.core.engine.registry.RegistryEntry;
 import io.crnk.core.engine.registry.RegistryEntryBuilder;
 import io.crnk.core.module.InitializingModule;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -14,6 +14,7 @@ import javax.inject.Inject;
 @Component
 @ConditionalOnBean(ExternalResourceProvider.class)
 public class ExternalTypeRepoInvoker implements InitializingModule {
+
   @Inject
   private ExternalResourceProvider externalResourceProvider;
 
@@ -21,30 +22,19 @@ public class ExternalTypeRepoInvoker implements InitializingModule {
 
   @Override
   public void init() {
-    externalResourceProvider.getTypes().forEach(aClass -> {
-      RegistryEntryBuilder builder = context.newRegistryEntryBuilder();
+    externalResourceProvider.getTypes().forEach(aClass ->
+      context.addRegistryEntry(getRegistryEntryForType(aClass, context.newRegistryEntryBuilder())));
+    context.addRegistryEntry(
+      getRegistryEntryForType("external-type", context.newRegistryEntryBuilder()));
+  }
 
-      RegistryEntryBuilder.ResourceRepositoryEntryBuilder resourceRepository = builder.resourceRepository();
-      resourceRepository.instance(new ExternalRepository(aClass));
-
-      InformationBuilder.ResourceInformationBuilder resource = builder.resource();
-      resource.resourceType(aClass);
-      resource.implementationType(ExternalRelationDto.class);
-      resource.addField("id", ResourceFieldType.ID, String.class);
-
-      context.addRegistryEntry(builder.build());
-    });
-    RegistryEntryBuilder builder = context.newRegistryEntryBuilder();
-
-    RegistryEntryBuilder.ResourceRepositoryEntryBuilder resourceRepository = builder.resourceRepository();
-    resourceRepository.instance(new ExternalRepository("external-type"));
-
-    InformationBuilder.ResourceInformationBuilder resource = builder.resource();
-    resource.resourceType("external-type");
-    resource.implementationType(ExternalRelationDto.class);
-    resource.addField("id", ResourceFieldType.ID, String.class);
-
-    context.addRegistryEntry(builder.build());
+  private static RegistryEntry getRegistryEntryForType(String type, RegistryEntryBuilder builder) {
+    builder.resourceRepository().instance(new ExternalRepository(type));
+    builder.resource()
+      .resourceType(type)
+      .implementationType(ExternalRelationDto.class)
+      .addField("id", ResourceFieldType.ID, String.class);
+    return builder.build();
   }
 
   @Override
