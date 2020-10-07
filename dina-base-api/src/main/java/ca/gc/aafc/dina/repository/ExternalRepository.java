480e50542e953f15fab536831c7eb67b3ed0739a
Jkeyuk <Jonkero2@gmail.com>
2020-10-07 11:46:14 -0400

    20489 external relations in response
    ExternalRepository polished up

--- dina-base-api/src/main/java/ca/gc/aafc/dina/repository/ExternalRepository.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/repository/ExternalRepository.java
@@ -1,20 +1,16 @@
 package ca.gc.aafc.dina.repository;
 
 import ca.gc.aafc.dina.dto.ExternalRelationDto;
+import io.crnk.core.exception.MethodNotAllowedException;
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.UntypedResourceRepository;
 import io.crnk.core.resource.annotations.JsonApiExposed;
-import io.crnk.core.resource.list.DefaultResourceList;
 import io.crnk.core.resource.list.ResourceList;
-import lombok.SneakyThrows;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @JsonApiExposed(false)
-public class ExternalRepository
-  implements UntypedResourceRepository<ExternalRelationDto, String> {
+public class ExternalRepository implements UntypedResourceRepository<ExternalRelationDto, String> {
 
   private final String resourceType;
 
@@ -23,39 +19,35 @@ public class ExternalRepository
   }
 
   @Override
+  public ExternalRelationDto findOne(String id, QuerySpec querySpec) {
+    return ExternalRelationDto.builder().type(resourceType).id(id).build();
+  }
+
+  @Override
   public ResourceList<ExternalRelationDto> findAll(QuerySpec querySpec) {
-    List<ExternalRelationDto> dtoList = querySpec.getFilters()
-      .stream()
-      .map(filterSpec -> newExternalType(filterSpec.getValue()))
-      .collect(Collectors.toList());
-    return new DefaultResourceList<>(dtoList, null, null);
+    throw new MethodNotAllowedException("External Types only use findOne");
   }
 
   @Override
   public ResourceList<ExternalRelationDto> findAll(
     Collection<String> collection, QuerySpec querySpec
   ) {
-    return findAll(querySpec);
+    throw new MethodNotAllowedException("External Types only use findOne");
   }
 
   @Override
   public <S extends ExternalRelationDto> S save(S s) {
-    return s;
+    throw new MethodNotAllowedException("External Types cannot be patched");
   }
 
   @Override
   public <S extends ExternalRelationDto> S create(S s) {
-    return s;
+    throw new MethodNotAllowedException("External Types cannot be created");
   }
 
   @Override
   public void delete(String serializable) {
-
-  }
-
-  @SneakyThrows
-  private ExternalRelationDto newExternalType(String id) {
-    return ExternalRelationDto.builder().type(resourceType).id(id).build();
+    throw new MethodNotAllowedException("External Types cannot be deleted");
   }
 
   @Override
@@ -68,10 +60,4 @@ public class ExternalRepository
     return ExternalRelationDto.class;
   }
 
-  @Override
-  public ExternalRelationDto findOne(
-    String serializable, QuerySpec querySpec
-  ) {
-    return newExternalType(serializable);
-  }
 }
