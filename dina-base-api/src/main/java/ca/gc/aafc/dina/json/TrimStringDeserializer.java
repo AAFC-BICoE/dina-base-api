7b5c54dacb5e4ab8c05424d6da03c7c1427619e5
Christian Gendreau <cgendreau@users.noreply.github.com>
2020-10-30 12:19:06 -0400

    Support #20754 Trim all strings on Jackson deserialize (#132)
    Implemented using Jackson StringDeserializer and tested using SpringBoot provided ObjectMapper

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/json/TrimStringDeserializer.java
@@ -0,0 +1,23 @@
+package ca.gc.aafc.dina.json;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+
+/**
+ * Jackson String deserializer to trim all strings to avoid having to deal with them downstream.
+ * There is no know use cases where leading and trailing whitespaces should be preserved.
+ */
+@JsonComponent
+public class TrimStringDeserializer extends StringDeserializer {
+
+  @Override
+  public String deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+    String value = super.deserialize(p, ctxt);
+    return value != null ? value.trim() : null;
+  }
+
+}
