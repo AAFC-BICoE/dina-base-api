755b49d7895e19ceaa2e344e02cb3b49c6fa4849
Jkeyuk <Jonkero2@gmail.com>
2020-11-09 11:21:13 -0500

    20819 method on custom field resolver
    add javadocs for CustomFieldResolver

--- dina-base-api/src/main/java/ca/gc/aafc/dina/mapper/CustomFieldResolver.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/mapper/CustomFieldResolver.java
@@ -5,10 +5,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a field to be resolved using a custom field mapping indicated by the annotations given
+ * custom setter method. The setter method refers to the method will set the apply the custom
+ * mapping. A field resolver should always have a parameter type that matches the source class of
+ * the mapping. A field resolver return types must also match the mapping target field type.
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CustomFieldResolver {
 
+  /**
+   * Name of the method that will apply the custom field mapping. A field resolver should always
+   * have a parameter type that matches the source class of the mapping. A field resolver return
+   * types must also match the mapping target field type.
+   *
+   * @return Name of the setter method.
+   */
   String setterMethod();
 
 }
+
