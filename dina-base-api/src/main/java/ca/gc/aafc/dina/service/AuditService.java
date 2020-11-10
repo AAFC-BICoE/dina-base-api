9408de89ce7c934a25e09ed6f918a6ba72674e30
Jkeyuk <Jonkero2@gmail.com>
2020-11-10 13:47:40 -0500

    20832 support removal of audit snapshots
    first iteration of the solution using base sql

--- dina-base-api/src/main/java/ca/gc/aafc/dina/service/AuditService.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/AuditService.java
@@ -7,6 +7,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.javers.core.Javers;
+import org.javers.core.commit.CommitId;
 import org.javers.core.metamodel.object.CdoSnapshot;
 import org.javers.repository.jql.QueryBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -15,7 +16,9 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -32,7 +35,7 @@ public class AuditService {
   /**
    * Returns a list of Audit snapshots filtered by a given instance and author.
    * Author and instance can be null for un-filtered results.
-   * 
+   *
    * @param instance - instance to filter may be null
    * @param author   - author to filter may be null
    * @param limit    - limit of results
@@ -46,7 +49,7 @@ public class AuditService {
   /**
    * Get the total resource count by a given Author and Audit Instance. Author and
    * instance can be null for un-filtered counts.
-   * 
+   *
    * @param author   - author to filter
    * @param instance - instance to filter
    * @return the total resource count
@@ -63,7 +66,7 @@ public class AuditService {
   /**
    * Commit a snapshot for a given object, A dina authenticated user will be set
    * as the commit author. If a user is not present the author will be anonymous.
-   * 
+   *
    * @param obj - domain object state to persist
    */
   public void audit(@NonNull Object obj) {
@@ -78,7 +81,7 @@ public class AuditService {
    * Commit a shallow delete snapshot for a given object, A dina authenticated
    * user will be set as the commit author. If a user is not present the author
    * will be anonymous.
-   * 
+   *
    * @param obj - domain object state to persist
    */
   public void auditDeleteEvent(@NonNull Object obj) {
@@ -93,7 +96,7 @@ public class AuditService {
    * Returns a list of Audit snapshots using a given Javers Facade filtered by a
    * given instance and author. Author and instance can be null for un-filtered
    * results.
-   * 
+   *
    * @param javers   - Facade to query
    * @param instance - instance to filter may be null
    * @param author   - author to filter may be null
@@ -123,7 +126,7 @@ public class AuditService {
   /**
    * Get the total resource count by a given Author and/or Audit Instance. Author
    * and instance can be null for un-filtered counts.
-   * 
+   *
    * @param jdbc     - NamedParameterJdbcTemplate for the query
    * @param author   - author to filter
    * @param instance - instance filter to apply
@@ -153,13 +156,31 @@ public class AuditService {
     @NonNull AuditInstance instance,
     @NonNull Javers javers
   ) {
+    List<CdoSnapshot> snapshots = javers.findSnapshots(
+      QueryBuilder.byInstanceId(instance.getId(), instance.getType()).build());
+    for (CdoSnapshot snap : snapshots) {
+      CommitId commitId = snap.getCommitId();
+      MapSqlParameterSource idMap = new MapSqlParameterSource(Map.of("id", commitId.valueAsNumber()));
 
+      String snapShotDelete = "delete from jv_snapshot where commit_fk = (select commit_pk from jv_commit where commit_id = :id)";
+      jdbc.update(snapShotDelete, idMap);
+
+      String globalDelete = "delete from jv_global_id where local_id = :id and type_name = :type";
+      jdbc.update(globalDelete, new MapSqlParameterSource(
+        Map.of("id", instance.getId(), "type", instance.getType())));
+
+      String commitDelete = "delete from jv_commit where commit_id = :id";
+      jdbc.update(commitDelete, idMap);
+
+      String commitPropertiesDelete = "delete from jv_commit_property where commit_fk = :id;";
+      jdbc.update(commitPropertiesDelete, idMap);
+    }
   }
 
   /**
    * Returns the needed SQL String to return a resouce count for a specific author
    * and id. Author and id can be null for un-filtered counts.
-   * 
+   *
    * @param author - author filter to apply
    * @param id     - id filter to apply
    * @return SQL String to return a resouce count
@@ -186,7 +207,7 @@ public class AuditService {
     /**
      * Returns an Optional AuditInstance from a string representation or empty if the
      * string is blank. Expected string format is {type}/{id}.
-     * 
+     *
      * @param instanceString - string to parse
      * @throws IllegalArgumentException if the string has an invalid format.
      * @return Optional AuditInstance or empty for blank strings
