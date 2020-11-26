9fc19e39cfcd6e088a55a47592d2aeb912f611db
Jkeyuk <Jonkero2@gmail.com>
2020-11-26 09:59:50 -0500

    21013 field adapters
    update test case for mappings of a single resolver

--- dina-base-api/src/test/java/ca/gc/aafc/dina/mapper/DinaFieldAdapterHandlerTest.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/mapper/DinaFieldAdapterHandlerTest.java
@@ -1,25 +1,55 @@
 package ca.gc.aafc.dina.mapper;
 
 import lombok.Builder;
+import lombok.Data;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
+
+import java.util.Set;
 
 class DinaFieldAdapterHandlerTest {
 
-  @Test
-  void resolveFields_FieldsResolved() {
+  private static final DinaFieldAdapterHandler handler = new DinaFieldAdapterHandler();
 
+  @Test
+  void resolveFields_ToDto_FieldsResolved() {
+    Car entity = Car.builder()
+      .name(RandomStringUtils.randomAlphabetic(5))
+      .powerLevel(9000)
+      .build();
+    CarDto dto = CarDto.builder().build();
+    handler.resolveFields(Set.of("customField"), entity, dto);
+    Assertions.assertEquals(entity.getPowerLevel(), dto.getCustomField());
+  }
+
+  @Test
+  void resolveFields_ToEntity_FieldsResolved() {
+    CarDto dto = CarDto.builder()
+      .name(RandomStringUtils.randomAlphabetic(5))
+      .powerLevel(9000)
+      .build();
+    Car entity = Car.builder().build();
+    handler.resolveFields(Set.of("customField"), dto, entity);
+    Assertions.assertEquals(dto.getName(), entity.getCustomField());
   }
 
   @Builder
+  @Data
   static class Car {
     String name;
     int powerLevel;
+    // uses dto name
+    String customField;
   }
 
   @Builder
+  @Data
   static class CarDto {
     String name;
     int powerLevel;
+    // uses entity power level
+    int customField;
   }
 
 }
