be8322cd78c119dd7f3f72a241ad5feb348f93df
Jkeyuk <jonkero2@gmail.com>
2021-02-12 10:34:46 -0500

    21724 dina role enum support for keycloak role
    User enum to hold the key cloak definition

--- dina-base-api/src/main/java/ca/gc/aafc/dina/security/DinaRole.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/security/DinaRole.java
@@ -1,19 +1,25 @@
 package ca.gc.aafc.dina.security;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.Optional;
 import java.util.regex.Pattern;
 
 /**
  * Represent user role in the context of a DINA module.
  */
+@RequiredArgsConstructor
 public enum DinaRole {
 
-  COLLECTION_MANAGER,
-  STAFF,
-  STUDENT,
-  DINA_ADMIN;
+  COLLECTION_MANAGER("collection-manager"),
+  STAFF("staff"),
+  STUDENT("student"),
+  DINA_ADMIN("dina-admin");
 
   private static final Pattern NON_ALPHA = Pattern.compile("[^A-Za-z]");
+  @Getter
+  private final String keyCloakDefinition;
 
   /**
    * Similar but more lenient than {@link #valueOf(String)}.
