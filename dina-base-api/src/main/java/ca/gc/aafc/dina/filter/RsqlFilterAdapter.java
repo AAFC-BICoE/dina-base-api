cbe47fbdd1787de7f59668d177981c843987b7e3
Jkeyuk <jonkero2@gmail.com>
2021-03-03 13:28:19 -0500

    21912 improve custom filters for rsql
    add java doc and update specs

--- dina-base-api/src/main/java/ca/gc/aafc/dina/filter/RsqlFilterAdapter.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/filter/RsqlFilterAdapter.java
@@ -7,6 +7,13 @@ import cz.jirutka.rsql.parser.ast.Node;
  */
 public interface RsqlFilterAdapter {
 
+  /**
+   * Entry point to process the rsql filters. This method will receive the root node of the rsql filters and
+   * will return the final rsql node to be used in the filtering process.
+   *
+   * @param node root node of the rsql filters
+   * @return final node to be used in the filtering process cannot be null.
+   */
   Node process(Node node);
 
 }
