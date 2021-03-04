4cf657d70003d51f951234a6b454a615a3730152
Jkeyuk <jonkero2@gmail.com>
2021-03-04 08:13:49 -0500

    Make simple filter handler static

--- dina-base-api/src/main/java/ca/gc/aafc/dina/filter/SimpleFilterHandler.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/filter/SimpleFilterHandler.java
@@ -1,38 +1,32 @@
 package ca.gc.aafc.dina.filter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.From;
-import javax.persistence.criteria.Predicate;
-
-import com.github.tennaito.rsql.misc.ArgumentParser;
-
 import ca.gc.aafc.dina.repository.SelectionHandler;
+import com.github.tennaito.rsql.misc.ArgumentParser;
 import io.crnk.core.queryspec.FilterOperator;
 import io.crnk.core.queryspec.FilterSpec;
 import io.crnk.core.queryspec.QuerySpec;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.From;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
- * Simple filter handler for filtering by a value in a single attribute.
- * Example GET request where pcrPrimer's [name] == '101F' :
- *   http://localhost:8080/api/pcrPrimer?filter[name]=101F
+ * Simple filter handler for filtering by a value in a single attribute. Example GET request where pcrPrimer's
+ * [name] == '101F' : http://localhost:8080/api/pcrPrimer?filter[name]=101F
  */
-@Named
-//CHECKSTYLE:OFF AnnotationUseStyle
-@RequiredArgsConstructor(onConstructor_ = @Inject)
-public class SimpleFilterHandler {
+public final class SimpleFilterHandler {
 
-  private final ArgumentParser argumentParser;
-
-  public Predicate getRestriction(QuerySpec querySpec, From<?, ?> root, CriteriaBuilder cb) {
+  public static Predicate getRestriction(
+    @NonNull QuerySpec querySpec,
+    @NonNull From<?, ?> root,
+    @NonNull CriteriaBuilder cb,
+    @NonNull ArgumentParser argumentParser
+  ) {
     List<FilterSpec> filterSpecs = querySpec.getFilters();
     List<Predicate> predicates = new ArrayList<>();
 
@@ -45,29 +39,28 @@ public class SimpleFilterHandler {
         // like "rsql" or others that are only handled by other FilterHandlers.
         continue;
       }
-      
-      predicates.add(generatePredicate(filterSpec, attributePath, cb));
+
+      predicates.add(generatePredicate(filterSpec, attributePath, cb, argumentParser));
     }
 
-    return cb.and(predicates.stream().toArray(Predicate[]::new));
+    return cb.and(predicates.toArray(Predicate[]::new));
   }
 
   /**
-   * Generates a predicate for a given crnk filter spec for a given attribute
-   * path. Predicate is built with the given criteria builder.
+   * Generates a predicate for a given crnk filter spec for a given attribute path. Predicate is built with
+   * the given criteria builder.
    *
-   * @param filter
-   *                        - filter to parse
-   * @param attributePath
-   *                        - path to the attribute
-   * @param cb
-   *                        - criteria builder to build the predicate
+   * @param filter         - filter to parse
+   * @param attributePath  - path to the attribute
+   * @param cb             - criteria builder to build the predicate
+   * @param argumentParser - the argument parser
    * @return a predicate for a given crnk filter spec
    */
-  private Predicate generatePredicate(
+  private static Predicate generatePredicate(
     @NonNull FilterSpec filter,
     @NonNull Expression<?> attributePath,
-    @NonNull CriteriaBuilder cb
+    @NonNull CriteriaBuilder cb,
+    @NonNull ArgumentParser argumentParser
   ) {
     // Convert the value to the target type:
     Object value = argumentParser.parse(
@@ -75,7 +68,7 @@ public class SimpleFilterHandler {
       attributePath.getJavaType()
     );
     if (value == null) {
-      return filter.getOperator() == FilterOperator.NEQ 
+      return filter.getOperator() == FilterOperator.NEQ
         ? cb.isNotNull(attributePath)
         : cb.isNull(attributePath);
     } else {
