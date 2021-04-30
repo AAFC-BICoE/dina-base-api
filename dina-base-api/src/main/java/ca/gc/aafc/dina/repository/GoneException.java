32b3453cbad6119afe29a799adb1d72a354ac277
Jkeyuk <jonkero2@gmail.com>
2021-04-30 11:40:54 -0400

    22500 add soft delete support to dina repo
    Gone exception can now build a meta section for the response

--- dina-base-api/src/main/java/ca/gc/aafc/dina/repository/GoneException.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/repository/GoneException.java
@@ -3,6 +3,8 @@ package ca.gc.aafc.dina.repository;
 import io.crnk.core.engine.document.ErrorData;
 import io.crnk.core.exception.CrnkMappableException;
 
+import java.util.Map;
+
 /**
  * Crnk Exception for HTTP 410 : GONE
  *
@@ -12,11 +14,16 @@ public class GoneException extends CrnkMappableException {
   private static final long serialVersionUID = 2658981047986565140L;
 
   public GoneException(String title, String message) {
+    this(title, message, null);
+  }
+
+  public GoneException(String title, String message, Map<String, Object> meta) {
     super(410, ErrorData.builder()
-        .setTitle(title)
-        .setDetail(message)
-        .setStatus("410")
-        .build());
+      .setTitle(title)
+      .setDetail(message)
+      .setStatus("410")
+      .setMeta(meta)
+      .build());
   }
 
 }
