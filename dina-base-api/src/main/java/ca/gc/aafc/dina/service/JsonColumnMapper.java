86a42b1eb0390919cae7682e6a0e658e9686cacd
Dmitri Karpinski <dimitri.karpinski@canada.ca>
2021-05-14 13:46:03 -0400

    checking only if key exist as a top-level key within the JSON value

--- dina-base-api/src/main/java/ca/gc/aafc/dina/service/JsonColumnMapper.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/JsonColumnMapper.java
@@ -6,33 +6,28 @@ import org.apache.ibatis.annotations.Select;
 
 /**
  * Base Managed Json Column Mapper Service based on mybatis (supports only PostgreSQL 9+)
- * The service checks every key in all nested objects from the json column
- * based on recursive CTE(https://www.postgresql.org/docs/9.4/queries-with.html)
+ * The service check if key exist as a top-level key within the JSON value
  * Returns total count of records which use the key in json column
  */
 @Mapper
 public interface JsonColumnMapper {
-    // This query is checking every key in all nested objects from the json column
-    String postgreSql = "SELECT COUNT(${columnName}) FROM ${tableName}" +
-            " WHERE #{keyName} IN (" +
-            " WITH RECURSIVE t(k,j) as (" +
-            " SELECT jsonb_object_keys(${columnName}), ${columnName}" +
-            " UNION ALL" +
-            " SELECT jsonb_object_keys(t.j->t.k), t.j->t.k" +
-            " FROM t WHERE jsonb_typeof(t.j->t.k) = 'object'" +
-            " )" +
-            " SELECT k FROM t)";
+    // This query is checking if the the key name exist as a top-level key within the JSON value
+    String sql = "SELECT COUNT(${columnName}) FROM ${tableName}" +
+            " WHERE ${columnName} ?? #{keyName}";
+    // RE: WHERE ${columnName} ?? #{keyName}
+    // we need to keep such question marks in a SQL statement from being interpreted as positional parameters
+    // that's why we have to use two question marks (??) as escape sequence
 
     /**
-     * Return number of records in the table which have specified key name in JSONb column
+     * Return number of records in the table if specified key exist as a top-level key within the JSON value
      *
      * @param tableName Table name against which the search will be performed
      * @param colName Column name against which the search will be performed
      * @param keyName Key name which will be searched
-     * @return Integer, number of records containing the key in the specified table with specified column
+     * @return Integer, number of records containing specified key exist as a top-level key within the JSON value
      */
-    @Select(postgreSql)
-    Integer countKeys (
+    @Select(sql)
+    Integer countFirstLevelKeys (
             @Param("tableName") String tableName,
             @Param("columnName") String colName,
             @Param("keyName") String keyName
