e268b78d7865811f98cf4404a54429e21538e879
Dmitri Karpinski <dimitri.karpinski@canada.ca>
2021-05-14 15:10:48 -0400

    since sql is no longer long using direct string

--- dina-base-api/src/main/java/ca/gc/aafc/dina/service/JsonColumnMapper.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/JsonColumnMapper.java
@@ -11,13 +11,6 @@ import org.apache.ibatis.annotations.Select;
  */
 @Mapper
 public interface JsonColumnMapper {
-    // This query is checking if the the key name exist as a top-level key within the JSON value
-    String postgreSql = "SELECT COUNT(${columnName}) FROM ${tableName}" +
-            " WHERE ${columnName} ?? #{keyName}";
-    // RE: WHERE ${columnName} ?? #{keyName}
-    // we need to keep such question marks in a SQL statement from being interpreted as positional parameters
-    // that's why we have to use two question marks (??) as escape sequence
-
     /**
      * Return number of records in the table if specified key exist as a top-level key within the JSON value
      *
@@ -26,10 +19,14 @@ public interface JsonColumnMapper {
      * @param keyName Key name which will be searched
      * @return Integer, number of records containing specified key exist as a top-level key within the JSON value
      */
-    @Select(postgreSql)
+    @Select("SELECT COUNT(${columnName}) FROM ${tableName} WHERE ${columnName} ?? #{keyName}")
     Integer countFirstLevelKeys (
             @Param("tableName") String tableName,
             @Param("columnName") String colName,
             @Param("keyName") String keyName
     );
+    // This query is checking if the the key name exist as a top-level key within the JSON value
+    // RE: WHERE ${columnName} ?? #{keyName}
+    // we need to keep such question marks in a SQL statement from being interpreted as positional parameters
+    // that's why we have to use two question marks (??) as escape sequence
 }
