1a62309e9315583f5d34aeb3616b813208a27fd0
Dmitri Karpinski <dimitri.karpinski@canada.ca>
2021-05-19 09:03:00 -0400

    RE: 22488_generic_jsonb_lookup_function https://redmine.biodiversity.agr.gc.ca/issues/22488 Changed old block indentation: +4 spaces with block indentation: +2 spaces to comply with existing Java Style Guide Removed internal link from java docs

--- dina-base-api/src/test/java/ca/gc/aafc/dina/service/PostgresJsonbServiceIT.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/service/PostgresJsonbServiceIT.java
@@ -1,53 +1,51 @@
 package ca.gc.aafc.dina.service;
 
-// Dina
 import ca.gc.aafc.dina.TestDinaBaseApp;
 import ca.gc.aafc.dina.service.PostgresJsonbService;
 import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
-// springframework
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-// tests
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-// utils
+
 import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = TestDinaBaseApp.class)
 @ContextConfiguration(initializers = { PostgresTestContainerInitializer.class })
 public class PostgresJsonbServiceIT {
 
-    @Inject
-    private PostgresJsonbService postgresJsonbService;
+  @Inject
+  private PostgresJsonbService postgresJsonbService;
 
-    @DisplayName("Test countFirstLevelKeys when key exist in JSONb column")
-    @Test
-    public void runTest_countFirstLevelKeysKeyExistInJson() {
-        // Run a mapper that would only work in postgres
-        assertEquals(
-                // attr_01 does exist in one record in jdata column
-                Integer.valueOf(1),
-                postgresJsonbService.countFirstLevelKeys(
-                        "dina_test_table",
-                        "jdata",
-                        "attr_01"
-                )
-        );
-    }
+  @DisplayName("Test countFirstLevelKeys when key exist in JSONb column")
+  @Test
+  public void countFirstLevelKeys_KeyExistInJson() {
+    // Run a countFirstLevelKeys that would only work in postgres
+    assertEquals(
+      // attr_01 does exist in one record in jdata column
+      Integer.valueOf(1),
+      postgresJsonbService.countFirstLevelKeys(
+        "dina_test_table",
+        "jdata",
+        "attr_01"
+      )
+    );
+  }
 
-    @DisplayName("Test countFirstLevelKeys when key NOT exist in JSONb column")
-    @Test
-    public void runTest_countFirstLevelKeysKeyNotExistInJson() {
-        // Run a mapper that would only work in postgres
-        assertEquals(
-                // attr_03 does not exist in any records in jdata column
-                Integer.valueOf(0),
-                postgresJsonbService.countFirstLevelKeys(
-                        "dina_test_table",
-                        "jdata",
-                        "attr_03"
-                )
-        );
-    }
+  @DisplayName("Test countFirstLevelKeys when key does not exist in JSONb column")
+  @Test
+  public void countFirstLevelKeys_KeyNotExistInJson() {
+    // Run countFirstLevelKeys that would only work in postgres
+    assertEquals(
+      // attr_03 does not exist in any records in jdata column
+      Integer.valueOf(0),
+      postgresJsonbService.countFirstLevelKeys(
+        "dina_test_table",
+        "jdata",
+        "attr_03"
+      )
+    );
+  }
 }
