9818b2833e21705197afceaeee029e2bf14a30b9
Christian Gendreau <christiangendreau@gmail.com>
2021-06-03 11:32:08 -0400

    Support #22328 Add validation resource Fixed invalid JavaDoc

--- dina-base-api/src/main/java/ca/gc/aafc/dina/repository/validation/ValidationResourceConfiguration.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/repository/validation/ValidationResourceConfiguration.java
@@ -3,15 +3,15 @@ package ca.gc.aafc.dina.repository.validation;
 import java.util.List;
 
 /**
- * Configuration design to return a list of {@link javax.xml.validation.ValidatorHandler} to be used by the
+ * Configuration design to return a list of {@link ValidationResourceHandler} to be used by the
  * validation repository.
  */
 public interface ValidationResourceConfiguration {
 
   /**
-   * Returns a list of @link javax.xml.validation.ValidatorHandler}
+   * Returns a list of {@link ValidationResourceHandler}
    *
-   * @return a list of @link javax.xml.validation.ValidatorHandler}
+   * @return a list of {@link ValidationResourceHandler}
    */
   List<ValidationResourceHandler<?>> getValidationHandlers();
 
