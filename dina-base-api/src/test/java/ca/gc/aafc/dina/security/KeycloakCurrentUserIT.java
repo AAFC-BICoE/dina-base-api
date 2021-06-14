fbfdebb0365ebad648b2e11865ac0a9f75d6d874
luusteve <45112541+luusteve@users.noreply.github.com>
2021-06-14 09:16:22 -0400

    23042 prepare dina security related classes (#217)
    * Move Security classes and added field priority to DinaRole
    
    * moveDinaRolePriorityComparator
    
    * spotbug fix
    
    * add methods isHigherThan and isHigherOrEqualThan
    
    * remove lines of code not in use
    
    * 23042 Prepare Dina Security related classes
    
    keep DinaRole priority private
    
    Co-authored-by: Christian Gendreau <christiangendreau@gmail.com>

--- dina-base-api/src/test/java/ca/gc/aafc/dina/security/KeycloakCurrentUserIT.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/security/KeycloakCurrentUserIT.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import javax.inject.Inject;
