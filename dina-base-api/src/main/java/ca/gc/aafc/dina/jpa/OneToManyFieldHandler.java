f51fef4e0611f62ba16d8a21bf3d686632d47a7f
Jkeyuk <jonkero2@gmail.com>
2021-06-14 08:09:53 -0400

    23014 generic way to resolve many to one fields
    Add java docs OneToManyFieldHandler.java

--- dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/OneToManyFieldHandler.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/OneToManyFieldHandler.java
@@ -8,6 +8,13 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+/**
+ * OneToManyFieldHandlers are used to resolve parent and child associations for One to Many relations during
+ * common create/update/delete operations.
+ *
+ * @param <C> Child type
+ * @param <P> Parent type
+ */
 @RequiredArgsConstructor
 public class OneToManyFieldHandler<C extends DinaEntity, P> {
 
@@ -17,10 +24,21 @@ public class OneToManyFieldHandler<C extends DinaEntity, P> {
   private final String parentFieldName;
   private final Consumer<C> orphanHandler;
 
+  /**
+   * Handles create operations to link associations of children to a given parent.
+   *
+   * @param parent parent with children to resolve
+   */
   public void onCreate(P parent) {
     OneToManyHibernateHelper.linkChildren(childSupplyMethod.apply(parent), parent, parentApplyMethod);
   }
 
+  /**
+   * Handles update operations to resolve associations of children to a given parent.
+   *
+   * @param parent      parent with children to resolve
+   * @param dinaService database service used to resolve current/incoming children
+   */
   public void onUpdate(P parent, DinaService<?> dinaService) {
     OneToManyHibernateHelper.handleOrphans(
       OneToManyHibernateHelper.findByParent(childClassType, parentFieldName, parent, dinaService),
@@ -30,6 +48,11 @@ public class OneToManyFieldHandler<C extends DinaEntity, P> {
     OneToManyHibernateHelper.linkChildren(childSupplyMethod.apply(parent), parent, parentApplyMethod);
   }
 
+  /**
+   * Handles delete operations to resolve orphans a given parent.
+   *
+   * @param parent parent with orphans to resolve
+   */
   public void onDelete(P parent) {
     OneToManyHibernateHelper.handleOrphans(childSupplyMethod.apply(parent), null, orphanHandler);
   }
