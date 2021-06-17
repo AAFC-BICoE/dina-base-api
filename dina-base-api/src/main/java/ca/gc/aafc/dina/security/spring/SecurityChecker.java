65329826e53b80fee58b1ed88387e9ab8d0706f9
Jkeyuk <jonkero2@gmail.com>
2021-06-17 14:40:15 -0400

    22804 permission in response
    add getPermissionsForObject

--- dina-base-api/src/main/java/ca/gc/aafc/dina/security/spring/SecurityChecker.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/security/spring/SecurityChecker.java
@@ -1,15 +1,19 @@
 package ca.gc.aafc.dina.security.spring;
 
 import ca.gc.aafc.dina.security.DinaAuthorizationService;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.security.access.expression.ExpressionUtils;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.util.SimpleMethodInvocation;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Inspired by
@@ -39,5 +43,31 @@ public final class SecurityChecker {
       new SimpleMethodInvocation(as, triggerCheckMethod, entity));
 
     return ExpressionUtils.evaluateAsBoolean(parser.parseExpression(securityExpression), evaluationContext);
+  }
+
+  public Set<String> getPermissionsForObject(Object target, DinaAuthorizationService as) {
+    Set<String> permissions = new HashSet<>();
+    Method authorizeCreate = getMethod("authorizeCreate", as.getClass().getSuperclass());
+    Method authorizeUpdate = getMethod("authorizeUpdate", as.getClass().getSuperclass());
+    Method authorizeDelete = getMethod("authorizeDelete", as.getClass().getSuperclass());
+    if (this.check(as, getPreAuthorizeExpression(authorizeCreate), target, authorizeCreate)) {
+      permissions.add("create");
+    }
+    if (this.check(as, getPreAuthorizeExpression(authorizeUpdate), target, authorizeUpdate)) {
+      permissions.add("delete");
+    }
+    if (this.check(as, getPreAuthorizeExpression(authorizeDelete), target, authorizeCreate)) {
+      permissions.add("update");
+    }
+    return permissions;
+  }
+
+  private String getPreAuthorizeExpression(Method matchingMethod) {
+    PreAuthorize annotation = matchingMethod.getAnnotation(PreAuthorize.class);
+    return annotation.value();
+  }
+
+  private Method getMethod(String methodName, Class<?> aClass) {
+    return MethodUtils.getMatchingMethod(aClass, methodName, Object.class);
   }
 }
