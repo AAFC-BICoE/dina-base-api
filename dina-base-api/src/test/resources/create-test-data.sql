70b46f8e0b237fd4f6d3780f625d2d8e3916343b
Christian Gendreau <cgendreau@users.noreply.github.com>
2021-06-28 13:31:21 -0400

    Update create-test-data.sql
    Added uuid column

--- dina-base-api/src/test/resources/create-test-data.sql
+++ dina-base-api/src/test/resources/create-test-data.sql
@@ -32,6 +32,7 @@ VALUES (2, 'row_two', 2, '{"attr_02": "val_02"}');
 CREATE TABLE hierarchy_test_table
 (
     id integer NOT NULL,
+    uuid uuid NOT NULL,
     name text NOT NULL,
     parent_id integer,
     CONSTRAINT hierarchy_table_pkey PRIMARY KEY (id)
@@ -41,14 +42,11 @@ ALTER TABLE hierarchy_test_table ADD CONSTRAINT fk_parent
     FOREIGN KEY (parent_id) references hierarchy_test_table(id);
 
 INSERT INTO public.hierarchy_test_table(
-    id, name, parent_id)
+    id, uuid, name, parent_id)
 VALUES 
- 	(1, 'a1', NULL), 
-	(2, 'a2', 1),
-	(3, 'a3', 1),
-	(4, 'a4', 2),
-    (5, 'a5', 2),
-    (6, 'a6', NULL);
-
-
-
+   (1, '7e0c2c3a-8113-427c-b8ed-83247a82ba43', 'a1', NULL), 
+   (2, 'c779eea7-3e6b-42a2-9ce6-88ca66857dad', 'a2', 1),
+   (3, 'cdeec1e6-9e24-477a-a598-e32af0447d43', 'a3', 1),
+   (4, '42cb7ad4-6600-4067-90f2-87d81e045172', 'a4', 2),
+   (5, '61a75686-a964-451a-86de-b15a89cbcebd', 'a5', 2),
+   (6, 'ffe87eae-5a10-44d2-8beb-cd2db7b116b9', 'a6', NULL);
