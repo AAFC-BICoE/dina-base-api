1f6d1785b658fa15deca998b87bcdd97e8c8b964
Belisle-Leclerc, Melanie <melanie.belisle-leclerc@canada.ca>
2021-06-28 13:06:47 -0400

    23162 Test SQL query to load hierarchy

--- dina-base-api/src/test/java/ca/gc/aafc/dina/service/PostgresHierarchicalDataServiceIT.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/service/PostgresHierarchicalDataServiceIT.java
@@ -25,22 +25,22 @@ public class PostgresHierarchicalDataServiceIT{
   public void getHierarchy_validEntry_hierarchyRetrieved() throws SQLException {
     List<HierarchicalObject> hierarchy = postgresHierarchicalDataService
         .getHierarchy("5", "hierarchy_test_table", "id", "parent_id", "name");
+        
+    assertEquals("Hierarchy should have three elements", 3, hierarchy.size());
     
     HierarchicalObject first = hierarchy.get(0);
     HierarchicalObject second = hierarchy.get(1);
     HierarchicalObject third = hierarchy.get(2);
     
-    assertEquals("Hierarchy should have three elements", 3, hierarchy.size());
+    assertEquals(Integer.valueOf(5), first.getId());
+    assertEquals(Integer.valueOf(1),  first.getRank());
+    assertEquals("a5", first.getName());
     
-    assertEquals("", Integer.valueOf(5), first.getId());
-    assertEquals("", Integer.valueOf(1),  first.getRank());
-    assertEquals("", "a5", first.getName());
+    assertEquals(Integer.valueOf(2), second.getId());
     
-    assertEquals("", Integer.valueOf(2), second.getId());
-    
-    assertEquals("", Integer.valueOf(1), third.getId());
-    assertEquals("", Integer.valueOf(3), third.getRank());
-    assertEquals("", "a1", third.getName());
+    assertEquals(Integer.valueOf(1), third.getId());
+    assertEquals(Integer.valueOf(3), third.getRank());
+    assertEquals("a1", third.getName());
 
   }
 
