1488865635738ef1f005b5c75572451f78982c7d
Belisle-Leclerc, Melanie <melanie.belisle-leclerc@canada.ca>
2021-06-29 07:30:32 -0400

    23162 test sql query to load hierarchy

--- dina-base-api/src/main/java/ca/gc/aafc/dina/service/PostgresHierarchicalDataService.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/PostgresHierarchicalDataService.java
@@ -13,15 +13,19 @@ import org.apache.ibatis.mapping.StatementType;
 @Mapper
 public interface PostgresHierarchicalDataService {
 /**
-* @param id ID of the object for which we seek the hierarchy
-* @param tableName  Table name against which the search will be performed
-* @param idColumnName     Name of the column containing the object id
+* @param id                 ID of the object for which we seek the hierarchy
+* @param tableName          Table name against which the search will be performed
+* @param idColumnName       Name of the column containing the object id
 * @param uuidColumnName
-* @param nameColumnName   Name of the column containing the object name
+* @param nameColumnName     Name of the column containing the object name
 * @param parentIdColumnName Name of the column containing the parent id
+* @return List&lt;HierarchicalObject&gt  
+*                           HierarchicalObjects represent each of the ancestors of the 
+*                           target object X (represented by id param). HierarcicalObjects 
+*                           have attributes id, uuid, name, and rank of the ancestor 
+*                           where rank is the distance from X + 1
 **/
 
-  
 @Select(
 "WITH RECURSIVE get_hierarchy (id, parent_id, uuid, name, rank) AS ( "
     + "SELECT initial_t.${idColumnName}, initial_t.${parentIdColumnName}, initial_t.${uuidColumnName}, initial_t.${nameColumnName}, 1 "
