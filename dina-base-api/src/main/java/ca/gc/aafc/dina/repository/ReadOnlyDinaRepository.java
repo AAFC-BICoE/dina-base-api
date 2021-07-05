60cf8a01ff439fc7d6edfb7dcd1444db485f446a
Jkeyuk <jonkero2@gmail.com>
2021-07-05 13:48:37 -0400

    23281 attempt auth service non optional
    import

--- dina-base-api/src/main/java/ca/gc/aafc/dina/repository/ReadOnlyDinaRepository.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/repository/ReadOnlyDinaRepository.java
@@ -3,7 +3,6 @@ package ca.gc.aafc.dina.repository;
 import ca.gc.aafc.dina.entity.DinaEntity;
 import ca.gc.aafc.dina.filter.DinaFilterResolver;
 import ca.gc.aafc.dina.mapper.DinaMapper;
-import ca.gc.aafc.dina.security.spring.SecurityChecker;
 import ca.gc.aafc.dina.security.AllowAllAuthorizationService;
 import ca.gc.aafc.dina.service.DinaService;
 import io.crnk.core.exception.MethodNotAllowedException;
