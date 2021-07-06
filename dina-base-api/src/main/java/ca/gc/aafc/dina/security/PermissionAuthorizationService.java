2b6301c76543a2743d88218e3f4238483791b0ac
Jkeyuk <jonkero2@gmail.com>
2021-07-06 08:03:41 -0400

    23281 attempt auth service non optional
    add java doc

--- dina-base-api/src/main/java/ca/gc/aafc/dina/security/PermissionAuthorizationService.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/security/PermissionAuthorizationService.java
@@ -5,6 +5,9 @@ import ca.gc.aafc.dina.security.spring.SecurityChecker;
 import javax.inject.Inject;
 import java.util.Set;
 
+/**
+ * Authorization service that can return the permissions for an object. Extend this service to enable.
+ */
 public abstract class PermissionAuthorizationService implements DinaAuthorizationService {
 
   @Inject
