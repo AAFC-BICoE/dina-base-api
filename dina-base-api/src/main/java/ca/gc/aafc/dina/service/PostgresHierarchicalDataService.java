1cd7eb481b56fddae7bf72dc6bb45dfd933a825c
Christian Gendreau <cgendreau@users.noreply.github.com>
2021-07-07 14:29:37 -0400

    Update PostgresHierarchicalDataService.java

--- dina-base-api/src/main/java/ca/gc/aafc/dina/service/PostgresHierarchicalDataService.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/PostgresHierarchicalDataService.java
@@ -13,21 +13,20 @@ import org.apache.ibatis.mapping.StatementType;
 @Mapper
 public interface PostgresHierarchicalDataService {
 
-/**
-* @param id                 ID of the object for which we seek the hierarchy
-* @param tableName          Table name against which the search will be performed
-* @param idColumnName       Name of the column containing the object id
-* @param uuidColumnName
-* @param nameColumnName     Name of the column containing the object name
-* @param parentIdColumnName Name of the column containing the parent id
-* @return List&lt;HierarchicalObject&gt  
-*                           HierarchicalObjects represent each of the ancestors of the 
-*                           target object X (represented by id param). HierarcicalObjects 
-*                           have attributes id, uuid, name, and rank of the ancestor 
-*                           where rank is the distance from X + 1
-**/
-
-@Select(
+  /**
+  * @param id                 ID of the object for which we seek the hierarchy
+  * @param tableName          Table name against which the search will be performed
+  * @param idColumnName       Name of the column containing the object id
+  * @param uuidColumnName
+  * @param nameColumnName     Name of the column containing the object name
+  * @param parentIdColumnName Name of the column containing the parent id
+  * @return List&lt;HierarchicalObject&gt  
+  *                           HierarchicalObjects represent each of the ancestors of the 
+  *                           target object X (represented by id param). HierarcicalObjects 
+  *                           have attributes id, uuid, name, and rank of the ancestor 
+  *                           where rank is the distance from X + 1
+  **/
+  @Select(
   "WITH RECURSIVE get_hierarchy (id, parent_id, uuid, name, rank) AS ( "
     + "SELECT initial_t.${idColumnName}, initial_t.${parentIdColumnName}, initial_t.${uuidColumnName}, initial_t.${nameColumnName}, 1 "
     + "FROM ${tableName} AS initial_t where initial_t.${idColumnName} = ${id} " + "UNION ALL "
@@ -44,5 +43,4 @@ List<HierarchicalObject> getHierarchy(
     @Param("parentIdColumnName") String parentIdColumnName,
     @Param("nameColumnName") String nameColumnName
   );
-  
 }
