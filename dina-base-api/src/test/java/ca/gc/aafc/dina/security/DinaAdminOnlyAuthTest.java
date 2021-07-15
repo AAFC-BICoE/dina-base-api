d5d278f34e8ba6903f26259bea482dcd55f9671c
Jkeyuk <jonkero2@gmail.com>
2021-07-15 14:10:55 -0400

    23459 dina auth service get name
    update test name

--- dina-base-api/src/test/java/ca/gc/aafc/dina/security/DinaAdminOnlyAuthTest.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/security/DinaAdminOnlyAuthTest.java
@@ -108,7 +108,7 @@ public class DinaAdminOnlyAuthTest {
   }
 
   @Test
-  void getSimpleName() {
+  void getName() {
     Assertions.assertEquals("DinaAdminOnlyAuthorizationService", authorizationService.getName());
   }
 
