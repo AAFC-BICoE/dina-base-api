e744653b904caddee532c0a24b1bd088daaf2c4e
luusteve <45112541+luusteve@users.noreply.github.com>
2021-08-06 12:08:51 -0400

    23783 move vocabulary from collection to dina base (#251)
    * add VocabularyConfiguration
    * checkstyle fix

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/vocabulary/VocabularyConfiguration.java
@@ -0,0 +1,30 @@
+package ca.gc.aafc.dina.vocabulary;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class VocabularyConfiguration {
+  
+  private final Map<String, List<VocabularyElement>> vocabulary;
+
+  public VocabularyConfiguration(Map<String, List<VocabularyElement>> vocabulary) {
+    this.vocabulary = vocabulary;
+  }
+
+  public Map<String, List<VocabularyElement>> getVocabulary() {
+    return vocabulary;
+  }
+
+  @NoArgsConstructor
+  @Getter
+  @Setter
+  public static class VocabularyElement {
+    private String name;
+    private String term;
+    private Map<String, String> labels;
+  }
+}
