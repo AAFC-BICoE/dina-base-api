7324c8d82301f978f115ab36ed43b9294e2fe19c
Jkeyuk <jonkero2@gmail.com>
2021-08-10 08:23:04 -0400

    20893_mybatis_for_audit_service
    getResourceCount now filters by instance (id and type)

--- dina-base-api/src/main/java/ca/gc/aafc/dina/service/JaversDataService.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/JaversDataService.java
@@ -10,7 +10,8 @@ public interface JaversDataService {
   @Select({"<script>",
     "select count(*) from jv_snapshot s join jv_commit c on s.commit_fk = c.commit_pk where 1=1",
     "<if test='author != null'> and c.author = #{author} </if>",
-    "<if test='id != null and type != null'> and global_id_fk = (select global_id_pk from jv_global_id where local_id = #{id} and type_name = #{type}) </if>",
+    "<if test='id != null and type != null'> and global_id_fk = ",
+      "(select global_id_pk from jv_global_id where local_id = #{id} and type_name = #{type}) </if>",
     "</script>"})
   Long getResourceCount(@Param("id") String id, @Param("type") String type, @Param("author") String author);
 
