524f83808d6fee499c2ee1e7d01a26a464ecf586
Jkeyuk <jonkero2@gmail.com>
2021-08-10 12:33:05 -0400

    20893_mybatis_for_audit_service
    checkstyle fixes

--- dina-base-api/src/main/java/ca/gc/aafc/dina/service/JaversDataService.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/JaversDataService.java
@@ -27,7 +27,7 @@ public interface JaversDataService {
     "select count(*) from jv_snapshot s join jv_commit c on s.commit_fk = c.commit_pk where 1=1",
     "<if test='author != null'> and c.author = #{author} </if>",
     "<if test='id != null and type != null'> and global_id_fk = ",
-      "(select global_id_pk from jv_global_id where local_id = #{id} and type_name = #{type}) </if>",
+    "(select global_id_pk from jv_global_id where local_id = #{id} and type_name = #{type}) </if>",
     "</script>"})
   Long getResourceCount(@Param("id") String id, @Param("type") String type, @Param("author") String author);
 
@@ -42,9 +42,9 @@ public interface JaversDataService {
    */
   @Delete({"<script>",
     "<foreach item='commitID' index='index' collection='commitIds'>",
-      "delete from jv_snapshot where commit_fk = (select commit_pk from jv_commit where commit_id = #{commitID});",
-      "delete from jv_commit where commit_id = #{commitID};",
-      "delete from jv_commit_property where commit_fk = (select commit_pk from jv_commit where commit_id = #{commitID});",
+    "delete from jv_snapshot where commit_fk = (select commit_pk from jv_commit where commit_id = #{commitID});",
+    "delete from jv_commit where commit_id = #{commitID};",
+    "delete from jv_commit_property where commit_fk = (select commit_pk from jv_commit where commit_id = #{commitID});",
     "</foreach>",
     "delete from jv_global_id where local_id = #{instanceId} and type_name = #{type};",
     "</script>"})
