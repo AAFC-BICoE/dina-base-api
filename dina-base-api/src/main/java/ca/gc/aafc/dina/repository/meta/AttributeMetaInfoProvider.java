2d208ca1b82374943cd453a89406a5857697976a
Jonathan Keyuk <Jonkero2@gmail.com>
2021-08-30 08:10:41 -0400

    24230 non empty json meta info (#261)
    * add   @JsonInclude(JsonInclude.Include.NON_EMPTY)
    * update setWarning Collections.emptyMap()

--- dina-base-api/src/main/java/ca/gc/aafc/dina/repository/meta/AttributeMetaInfoProvider.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/repository/meta/AttributeMetaInfoProvider.java
@@ -1,13 +1,14 @@
 package ca.gc.aafc.dina.repository.meta;
 
 import ca.gc.aafc.dina.mapper.IgnoreDinaMapping;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.crnk.core.resource.annotations.JsonApiMetaInformation;
 import io.crnk.core.resource.meta.MetaInformation;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -27,14 +28,16 @@ public abstract class AttributeMetaInfoProvider {
   @Builder
   @Getter
   @Setter
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
   public static class DinaJsonMetaInfo implements MetaInformation {
+
     private String permissionsProvider;
     private Set<String> permissions;
     private Map<String, Object> warnings;
 
     public void setWarning(String warningKey, Object warningValue) {
       if (warnings == null) {
-        warnings = new HashMap<>();
+        warnings = Collections.emptyMap();
       }
       this.warnings.put(warningKey, warningValue);
     }
