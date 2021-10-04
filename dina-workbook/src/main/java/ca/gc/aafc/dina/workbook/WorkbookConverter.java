c40065da45e4b308b4f31a4637195c3a82b12156
Christian Gendreau <cgendreau@users.noreply.github.com>
2021-10-04 10:15:11 -0400

    24770 convert any sheet in workbook converter (#271)
    * Support #24770 convert any sheet in WorkbookConverter
    allow to specify a specific sheet to convert
    
    * Support #24770 convert any sheet in WorkbookConverter
    Added new method convertWorkbook
    
    * Support #24770 convert any sheet in WorkbookConverter
    Added deprecated javadoc annotation
    
    * Add files via upload
    
    Added Sheet1 to Workbook
    
    * 24770 Convert any sheet in WorkbookConverter
    Added test for workbook conversion

--- dina-workbook/src/main/java/ca/gc/aafc/dina/workbook/WorkbookConverter.java
+++ dina-workbook/src/main/java/ca/gc/aafc/dina/workbook/WorkbookConverter.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @SuppressFBWarnings({ "EI_EXPOSE_REP", "EI_EXPOSE_REP2" })
@@ -33,6 +35,8 @@ public final class WorkbookConverter {
   }
 
   /**
+   * @deprecated use convertWorkbook or convertSheet
+   *
    * Converts the first sheet of a Workbook into a list of WorkbookRow.
    * The method will use the string value of each cells.
    * The entire sheet will be loaded in memory.
@@ -41,11 +45,45 @@ public final class WorkbookConverter {
    * @return list of all rows or an empty list (never null)
    * @throws IOException
    */
+  @Deprecated
   public static List<WorkbookRow> convert(InputStream in) throws IOException {
-    List<WorkbookRow> sheetContent = new ArrayList<>();
-    Workbook book = WorkbookFactory.create(in);
-    Sheet sheet = book.getSheetAt(0); // only convert sheet 0
+    return convertSheet(in, 0);
+  }
 
+  /**
+   * Converts a Workbook and return a Map where the key is the sheet number (starting at 0) and
+   * the value a list of WorkbookRow.
+   * The method will use the string value of each cells.
+   * The entire workbook will be loaded in memory.
+   * @param in will not be closed by this method
+   * @return map of sheet and list of all rows or an empty map (never null)
+   * @throws IOException
+   */
+  public static Map<Integer, List<WorkbookRow>> convertWorkbook(InputStream in) throws IOException {
+    Map<Integer, List<WorkbookRow>> workbookContent = new HashMap<>();
+    Workbook book = WorkbookFactory.create(in);
+    for (int i = 0; i < book.getNumberOfSheets(); i++) {
+      workbookContent.put(i, convertSheet(book.getSheetAt(i)));
+    }
+    return workbookContent;
+  }
+
+  /**
+   * Converts a specific sheet of a Workbook into a list of WorkbookRow.
+   * The method will use the string value of each cells.
+   * The entire sheet will be loaded in memory.
+   * @param in will not be closed by this method
+   * @param sheetNumber sheet to concert, starts at 0
+   * @return list of all rows or an empty list (never null)
+   * @throws IOException
+   */
+  public static List<WorkbookRow> convertSheet(InputStream in, int sheetNumber) throws IOException {
+    Workbook book = WorkbookFactory.create(in);
+    return convertSheet(book.getSheetAt(sheetNumber));
+  }
+
+  private static List<WorkbookRow> convertSheet(Sheet sheet) {
+    List<WorkbookRow> sheetContent = new ArrayList<>();
     for (Row row : sheet) {
       String[] content = new String[row.getLastCellNum() > 0 ? row.getLastCellNum() : 0];
       for (int i = 0; i < row.getLastCellNum(); i++) {
@@ -58,7 +96,6 @@ public final class WorkbookConverter {
           .build();
       sheetContent.add(currWorkbookRow);
     }
-
     return sheetContent;
   }
 
--- dina-workbook/src/test/java/ca/gc/aafc/dina/workbook/WorkbookConverterIT.java
+++ dina-workbook/src/test/java/ca/gc/aafc/dina/workbook/WorkbookConverterIT.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -15,20 +16,32 @@ public class WorkbookConverterIT {
   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
   @Test
-  public void workbookConverter_onValidFile_ExpectedContentConverted() throws IOException {
+  public void workbookConverterSheet_onValidFile_ExpectedContentConverted() throws IOException {
 
     try (InputStream is = WorkbookConverterIT.class.getClassLoader()
         .getResourceAsStream("specimenImporterTemplateTest.xlsx")) {
-      List<WorkbookConverter.WorkbookRow> content = WorkbookConverter.convert(is);
+      List<WorkbookConverter.WorkbookRow> sheet = WorkbookConverter.convertSheet(is, 0);
 
-      assertEquals(3, content.size());
-      assertEquals("Collection Code *", content.get(0).getContent()[0]);
+      assertEquals(3, sheet.size());
+      assertEquals("Collection Code *", sheet.get(0).getContent()[0]);
 
-      String jsonStr = OBJECT_MAPPER.writeValueAsString(content);
+      String jsonStr = OBJECT_MAPPER.writeValueAsString(sheet);
 
       //null elements should be included as empty string
       assertFalse(jsonStr.contains("null"));
+    }
+  }
 
+  @Test
+  public void workbookConverterWorkbook_onValidFile_ExpectedContentConverted() throws IOException {
+
+    try (InputStream is = WorkbookConverterIT.class.getClassLoader()
+        .getResourceAsStream("specimenImporterTemplateTest.xlsx")) {
+      Map<Integer, List<WorkbookConverter.WorkbookRow>> workbook = WorkbookConverter.convertWorkbook(is);
+
+      assertEquals(2, workbook.size());
+      assertEquals("Collection Code *", workbook.get(0).get(0).getContent()[0]);
+      assertEquals("sheet", workbook.get(1).get(0).getContent()[0]);
     }
   }
 
@@ -36,7 +49,7 @@ public class WorkbookConverterIT {
   public void workbookConverter_onValidFileWithEmptyLine_ExpectedContentConverted() throws IOException {
     try (InputStream is = WorkbookConverterIT.class.getClassLoader()
         .getResourceAsStream("empty_first_row.xlsx")) {
-      List<WorkbookConverter.WorkbookRow> content = WorkbookConverter.convert(is);
+      List<WorkbookConverter.WorkbookRow> content = WorkbookConverter.convertSheet(is, 0);
       assertEquals(3, content.size());
       assertEquals("test1", content.get(0).getContent()[0]);
       assertEquals(1, content.get(0).getRowNumber());
@@ -47,7 +60,7 @@ public class WorkbookConverterIT {
   public void workbookConverter_onValidFileWithFormatting_ExpectedContentConverted() throws IOException {
     try (InputStream is = WorkbookConverterIT.class.getClassLoader()
         .getResourceAsStream("styled_spreadsheet.xlsx")) {
-      List<WorkbookConverter.WorkbookRow> content = WorkbookConverter.convert(is);
+      List<WorkbookConverter.WorkbookRow> content = WorkbookConverter.convertSheet(is, 0);
       assertEquals(49, content.size());
     }
   }
