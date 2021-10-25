2c4117b5d449cd9e33078a36eb5579f8f1deaf62
Jkeyuk <jonkero2@gmail.com>
2021-10-25 11:33:37 -0400

    25056_connect_jsonb_simple_filters
    remove old unused method

--- dina-base-api/src/main/java/ca/gc/aafc/dina/repository/SelectionHandler.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/repository/SelectionHandler.java
@@ -1,26 +1,24 @@
 package ca.gc.aafc.dina.repository;
 
-import java.util.List;
-
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.From;
 import javax.persistence.criteria.JoinType;
-
-import io.crnk.core.engine.registry.ResourceRegistry;
+import java.util.List;
 
 /**
  * Provides methods for handling sparse field sets and inclusion of related resources.
  */
 public final class SelectionHandler {
 
-  private SelectionHandler() { }
-  
+  private SelectionHandler() {
+  }
+
   /**
-   * Gets a JPA expression given a base path and an attributePath. Works as a short-hand method to
-   * get expressions that could require joins.
-   * This method could be rewritten later to map DTO fields to custom expressions.
-   * 
-   * @param basePath the base path
+   * Gets a JPA expression given a base path and an attributePath. Works as a short-hand method to get
+   * expressions that could require joins. This method could be rewritten later to map DTO fields to custom
+   * expressions.
+   *
+   * @param basePath      the base path
    * @param attributePath the attribute path
    * @return the expression
    */
@@ -31,30 +29,5 @@ public final class SelectionHandler {
     }
     return from.get(attributePath.get(attributePath.size() - 1));
   }
-  
-  /**
-   * Gets the name of the JPA entity's @Id attribute
-   * 
-   * @return
-   */
-  public static String getIdAttribute(Class<?> resourceClass, ResourceRegistry resourceRegistry) {
-    return resourceRegistry.findEntry(resourceClass)
-        .getResourceInformation()
-        .getIdField()
-        .getUnderlyingName();
-  }
-  
-  /**
-   * Gets the resource class' @JsonApiId attribute.
-   * 
-   * @return the JPA Expression of the Id attribute.
-   */
-  public static Expression<?> getIdExpression(
-      From<?, ?> root,
-      Class<?> resourceClass,
-      ResourceRegistry resourceRegistry
-  ) {
-    return root.get(getIdAttribute(resourceClass, resourceRegistry));
-  }
-  
+
 }
