c649c07dbd82ce2f5284ede298655ac4f5e4187c
Jkeyuk <jonkero2@gmail.com>
2021-11-01 12:55:56 -0400

    25206_improve_documentation_on_DinaRepository
    initial work to use jsonb_path_exists

--- dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/JsonbValueSpecification.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/JsonbValueSpecification.java
@@ -1,11 +1,13 @@
 package ca.gc.aafc.dina.jpa;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
+import java.util.stream.Stream;
 
 /**
  * Allows to create JPA Predicate matching a value on a specific jsonb key. PostgreSQL specific.
@@ -14,7 +16,7 @@ import javax.persistence.criteria.Predicate;
  */
 public class JsonbValueSpecification<T> {
 
-  static final String JSONB_EXTRACT_PATH_PG_FUNCTION_NAME = "jsonb_extract_path_text";
+  static final String JSONB_EXTRACT_PATH_PG_FUNCTION_NAME = "jsonb_path_exists";
 
   private final String columnName;
   private final String[] keys;
@@ -27,15 +29,27 @@ public class JsonbValueSpecification<T> {
   public Predicate toPredicate(Path<T> root, CriteriaBuilder builder, String value) {
     if (ArrayUtils.isNotEmpty(keys)) {
       Expression<?>[] literals = {root.get(this.columnName)};
-      for (String key : keys) {
-        literals = ArrayUtils.add(literals, builder.literal(key));
-      }
+
+      String path = "$[*]." + StringUtils.join(Stream.of(keys), ".") + " ? (@ == $val)";
+
+      Expression<Object> pathExpression = builder.function("cast", Object.class,
+        builder.literal(path + " as int ")); //TODO how to cast to JSONPATH ??
+
+      Expression<Object> valueExpression = builder.function("json_build_object", Object.class,
+        builder.literal("val"),
+        builder.literal(value));
+
+      Expression<Object> to_jsonb = builder.function("to_jsonb", Object.class, valueExpression);
+
+      literals = ArrayUtils.add(literals, builder.literal(path));
+      literals = ArrayUtils.add(literals, to_jsonb);
+
       return builder.equal(
         builder.function(
           JSONB_EXTRACT_PATH_PG_FUNCTION_NAME,
-          String.class,
+          Boolean.class,
           literals),
-        value);
+        true);
     }
     return builder.and();
   }
