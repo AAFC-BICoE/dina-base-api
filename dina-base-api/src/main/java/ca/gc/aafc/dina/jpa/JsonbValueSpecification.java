8f0103a27ceb324067d8e984300e92ed51f1673d
Jkeyuk <jonkero2@gmail.com>
2021-11-03 07:58:59 -0400

    25175_try_jsonb_array_predicate
    revert changes to JsonbValueSpecification.java

--- dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/JsonbValueSpecification.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/jpa/JsonbValueSpecification.java
@@ -1,42 +1,30 @@
 package ca.gc.aafc.dina.jpa;
 
-import org.apache.commons.lang3.ArrayUtils;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 /**
- * Allows to create JPA Predicate matching a value on a specific jsonb key. PostgreSQL specific.
- *
+ * Allows to create JPA Predicate matching a value on a specific jsonb key.
+ * PostgreSQL specific.
  * @param <T>
  */
+@RequiredArgsConstructor
 public class JsonbValueSpecification<T> {
 
   static final String JSONB_EXTRACT_PATH_PG_FUNCTION_NAME = "jsonb_extract_path_text";
 
   private final String columnName;
-  private final String[] keys;
+  private final String keyName;
 
-  public JsonbValueSpecification(String columnName, String... keys) {
-    this.columnName = columnName;
-    this.keys = keys;
-  }
-
-  public Predicate toPredicate(Path<T> root, CriteriaBuilder builder, String value) {
-    if (ArrayUtils.isNotEmpty(keys)) {
-      Expression<?>[] literals = {root.get(this.columnName)};
-      for (String key : keys) {
-        literals = ArrayUtils.add(literals, builder.literal(key));
-      }
-      return builder.equal(
-        builder.function(
-          JSONB_EXTRACT_PATH_PG_FUNCTION_NAME,
-          String.class,
-          literals),
+  public Predicate toPredicate(Root<T> root, CriteriaBuilder builder, String value) {
+    return builder.equal(builder
+        .function(JSONB_EXTRACT_PATH_PG_FUNCTION_NAME,
+            String.class,
+            root.get(this.columnName),
+            builder.literal(this.keyName)),
         value);
-    }
-    return builder.and();
   }
 }
