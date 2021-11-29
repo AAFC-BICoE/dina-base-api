08f2a901edda07ac252d840429955e81c93d949d
luusteve <luu.stevenluu@gmail.com>
2021-11-29 14:58:04 -0500

    Utitlize ISODateTime.parse

--- dina-base-api/src/main/java/ca/gc/aafc/dina/validation/ISOPartialDateValidator.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/validation/ISOPartialDateValidator.java
@@ -1,28 +1,26 @@
 package ca.gc.aafc.dina.validation;
 
-import java.util.Optional;
-import java.util.regex.Pattern;
+import java.time.format.DateTimeParseException;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import ca.gc.aafc.dina.datetime.ISODateTime;
 import ca.gc.aafc.dina.datetime.ISODateTime.Format;
 
 public class ISOPartialDateValidator implements ConstraintValidator<ISOPartialDate, String> {
 
-  private static final Pattern ALL_NON_NUMERIC = Pattern.compile("[^\\d]");
-
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
-    if (value != null) {
-      int numberOfNumericChar = ALL_NON_NUMERIC
-        .matcher(value).replaceAll("").length();
-
-      Optional<Format> format = Format.fromPrecision(numberOfNumericChar);
-  
-      return format.isPresent() && format.get().getPrecision() <= Format.YYYY_MM_DD.getPrecision();
-    } else {
+    try {
+      if (value != null) {
+        ISODateTime isoDateTime = ISODateTime.parse(value);
+        Format format = isoDateTime.getFormat();
+        return format.getPrecision() <= Format.YYYY_MM_DD.getPrecision();
+      }
       return true;
+    } catch (DateTimeParseException e) {
+      return false;
     }
   }
   
