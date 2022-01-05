ae2fb375e4dad5640553cb1144007078e5ff815c
Brandon Andre <andr0284@algonquinlive.com>
2022-01-05 14:57:44 -0500

    In-Progress - Support #26123 - More MessageProducer calls to afterCommit in Message
    https://redmine.biodiversity.agr.gc.ca/issues/26123
    
    - Added the TransactionSynchorizationManager to the sendMessage so we can take advantage of the afterCommit().
    - There's an issue with the test

--- dina-base-api/src/main/java/ca/gc/aafc/dina/service/MessageProducingService.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/MessageProducingService.java
@@ -1,11 +1,14 @@
 package ca.gc.aafc.dina.service;
 
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.validation.SmartValidator;
+
 import ca.gc.aafc.dina.entity.DinaEntity;
 import ca.gc.aafc.dina.jpa.BaseDAO;
 import ca.gc.aafc.dina.search.messaging.producer.MessageProducer;
 import ca.gc.aafc.dina.search.messaging.types.DocumentOperationNotification;
 import ca.gc.aafc.dina.search.messaging.types.DocumentOperationType;
-import org.springframework.validation.SmartValidator;
 
 public class MessageProducingService<E extends DinaEntity> extends DefaultDinaService<E> {
 
@@ -43,11 +46,21 @@ public class MessageProducingService<E extends DinaEntity> extends DefaultDinaSe
     sendMessage(entity, DocumentOperationType.DELETE);
   }
 
+  /**
+   * Sends a message only after the main transation is successful comitted.
+   * 
+   * @param persisted entity to be sent as a message.
+   * @param add operation type (example: DocumentOperationType.DELETE)
+   */
   private void sendMessage(E persisted, DocumentOperationType add) {
-    producer.send(DocumentOperationNotification.builder()
-      .operationType(add)
-      .documentId(persisted.getUuid().toString())
-      .documentType(resourceType)
-      .build());
+    TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization(){
+      public void afterCommit(){
+        producer.send(DocumentOperationNotification.builder()
+          .operationType(add)
+          .documentId(persisted.getUuid().toString())
+          .documentType(resourceType)
+          .build());
+      }
+    });
   }
 }
