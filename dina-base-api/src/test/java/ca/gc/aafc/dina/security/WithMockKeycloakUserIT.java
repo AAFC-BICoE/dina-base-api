01a7667adeece6b7b17b911f791a077a9477e957
Brandon Andre <andr0284@algonquinlive.com>
2022-01-17 08:32:53 -0500

    Added transactional annotation for testing

--- dina-base-api/src/test/java/ca/gc/aafc/dina/security/WithMockKeycloakUserIT.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/security/WithMockKeycloakUserIT.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Transactional
 @SpringBootTest(
   classes = TestDinaBaseApp.class,
   properties = "keycloak.enabled: true"
@@ -31,7 +33,7 @@ public class WithMockKeycloakUserIT {
   @WithMockKeycloakUser(groupRole = {"group 1:staff", "group 3:staff"})
   @Test
   public void create_AuthorizedGroup_CreatesObject() {
-    PersonDTO dto = PersonDTO.builder().uuid(UUID.randomUUID()).group(GROUP_1).name("test6").build();
+    PersonDTO dto = PersonDTO.builder().uuid(UUID.randomUUID()).group(GROUP_1).name("name").build();
     PersonDTO result = dinaRepository.create(dto);
     assertNotNull(result.getUuid());
   }
