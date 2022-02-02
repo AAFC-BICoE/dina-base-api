04732cfcf6803cabfdb6900813f2baa109822c1e
Christian Gendreau <christiangendreau@gmail.com>
2022-02-02 12:11:06 -0500

    25636 Update to SpringBoot 2.6.3 Fixed Checkstyle issues

--- dina-base-api/src/main/java/ca/gc/aafc/dina/security/KeycloakAuthConfig.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/security/KeycloakAuthConfig.java
@@ -1,8 +1,6 @@
 package ca.gc.aafc.dina.security;
 
 import lombok.extern.log4j.Log4j2;
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
