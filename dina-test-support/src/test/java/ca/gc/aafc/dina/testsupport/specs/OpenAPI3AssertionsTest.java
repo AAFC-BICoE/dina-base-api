dcbcae6cd5cefc2e3aae417130f41b12ec8931e3
Brandon Andre <andr0284@algonquinlive.com>
2022-04-04 14:44:10 -0400

    Support #25460 - Additional fields in nested object seem to be missed by the OpenApiIT (#326)
    - Decided to re-write the validator to allow for nested objects. There were a couple of problems with the old implementation.
    - Validator will now retrieve required and possible fields for each level.
    - If there is any fields that contain a "properties" then it will validate that level.
    - Cleaned up and fixed any issues with the json/yaml test resources. Changes some files to test more things.
    - allowableMissingFields now supports paths like "customObject/name/firstName" or just the name at the top level as before "customObject".
    - Removed unused logging/importing from RestrictiveFieldValidator.
    - Cleaned up the OpenAPI3AssertionsTest. Moved similar tests together and added comments.
    - Added schema to allow testing of remote specs but that will be under control of that repo.
    - The validate method actually get's ran at each level of the request being validated. Redesigned it and simplified the code to take advantage of this.
    - SchemaNode actually also get's updated to the exact level being tested.
    - If a field is added to the allowableMissingFields, it's children fields will now be skipped.
    - Added a containsAllowableMissingField helper method that is null safe.
    - Fixed some required fields in the tests that should be there.
    - Removed unneeded null check since the containsAllowableMIssingFields already handles it.
    
    Co-authored-by: @luusteve
    Co-authored-by: @cgendreau

--- dina-test-support/src/test/java/ca/gc/aafc/dina/testsupport/specs/OpenAPI3AssertionsTest.java
+++ dina-test-support/src/test/java/ca/gc/aafc/dina/testsupport/specs/OpenAPI3AssertionsTest.java
@@ -1,7 +1,7 @@
 package ca.gc.aafc.dina.testsupport.specs;
 
 import ca.gc.aafc.dina.testsupport.TestResourceHelper;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.opentest4j.AssertionFailedError;
 import org.springframework.http.HttpMethod;
@@ -15,22 +15,23 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
- * Test making sure we can parse, validate and assert a know OpenAPI 3 specification with a
- * predefined API response.
- *
+ * Test making sure we can parse, validate and assert a know OpenAPI 3
+ * specification with a predefined API response.
  */
 public class OpenAPI3AssertionsTest {
 
+  private static final String REMOTE_SPECS = "https://raw.githubusercontent.com/DINA-Web/object-store-specs/master/schema/object-store-api.yml";
+
   /**
    * Test making sure we can skip a schema validation by a system property.
-   * To simulate an ureachable URL we are simply using a non-existing url.
+   * To simulate an unreachable URL we are simply using a non-existing url.
    */
   @Test
-  public void assertRemoteSchemaTest() throws IOException {
+  public void assertRemoteSchema_InvalidURLWithSkipRemoteValidation_NoValidationIssues() throws IOException {
 
     URL specsUrl = new URL("http://abcd.abc");
     assertThrows(UnknownHostException.class, () -> {
-      ((HttpURLConnection)specsUrl.openConnection()).getResponseCode();
+      ((HttpURLConnection) specsUrl.openConnection()).getResponseCode();
     }, "Make sure specsUrl doesn't exist and is not reachable.");
 
     System.setProperty(OpenAPI3Assertions.SKIP_REMOTE_SCHEMA_VALIDATION_PROPERTY, "true");
@@ -39,102 +40,173 @@ public class OpenAPI3AssertionsTest {
     System.clearProperty(OpenAPI3Assertions.SKIP_REMOTE_SCHEMA_VALIDATION_PROPERTY);
   }
 
+  /**
+   * This test will actually use the object-store-api schema to validate a response. This test is
+   * used to indicate remote schema validation is working.
+   */
   @Test
-  public void assertSchemaTest() throws IOException {
+  public void assertRemoteSchema_ValidURLProvided_NoValidationIssues() throws IOException {
+    String responseJson = TestResourceHelper.readContentAsString("objectStoreManagedAttributeAPIResponse.json");
+    OpenAPI3Assertions.assertRemoteSchema(
+        new URL(REMOTE_SPECS),
+        "ManagedAttribute", responseJson);
+  }
+
+  /**
+   * Test to ensure the assertEndPoint is validating the HttpMethod of the endpoint without any
+   * issues.
+   */
+  @Test
+  public void assertEndPoint_ValidEndpoint_Successful() {
+    URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
+    OpenAPI3Assertions.assertEndpoint(specsUrl, "/v1/managed-attribute", HttpMethod.GET);
+  }
+
+  /**
+   * Test a valid json response against a schema. No validation exception should be provided.
+   */
+  @Test
+  public void assertSchema_CorrectResponse_NoValidationIssues() throws IOException {
     URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
     String responseJson = TestResourceHelper.readContentAsString("managedAttributeAPIResponse.json");
     OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson);
   }
 
+  /**
+   * All fields provided on the schema are considered required by design. This test will purposely
+   * remove a required field from the response to ensure an error is retrieved.
+   */
   @Test
-  public void assertSchema_WhenNonRequiredAttributeMissing() throws IOException {
+  public void assertSchema_WhenNonRequiredAttributeMissing_ExpectValidationIssues() throws IOException {
     URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
     String responseJson = TestResourceHelper.readContentAsString("missingAttribute.json");
     Assertions.assertThrows(
-      AssertionFailedError.class,
-      () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson));
+        AssertionFailedError.class,
+        () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson));
   }
 
+  /**
+   * All relationships provided on the schema are considered required by design. This test will
+   * purposely remove the "collectingEvent" relationship. A validation error is to be expected.
+   */
   @Test
-  public void assertSchema_WhenNonRequiredRelationMissing() throws IOException {
+  public void assertSchema_WhenNonRequiredRelationMissing_ExpectValidationIssues() throws IOException {
     URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
     String responseJson = TestResourceHelper.readContentAsString("missingRelation.json");
     Assertions.assertThrows(
-      AssertionFailedError.class,
-      () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson));
+        AssertionFailedError.class,
+        () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson));
   }
 
+  /**
+   * Any additional fields that are not specified in the schema are to be reported.
+   */
   @Test
-  public void assertSchema_WhenAttributeShouldNotExist() throws IOException {
+  public void assertSchema_WhenAttributeShouldNotExist_ExpectValidationIssues() throws IOException {
     URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
     String responseJson = TestResourceHelper.readContentAsString("fieldThatShouldNotExist.json");
     Assertions.assertThrows(
-      AssertionFailedError.class,
-      () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson));
+        AssertionFailedError.class,
+        () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson));
   }
 
+  /**
+   * With the allowAdditionalFields setting, any included fields which are not specified in the
+   * schema are ignored. No errors are expected here.
+   */
   @Test
-  public void assertSchema_AllowsAdditionalFields() throws IOException {
+  public void assertSchema_AllowsAdditionalFields_NoValidationIssues() throws IOException {
     URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
     String responseJson = TestResourceHelper.readContentAsString("fieldThatShouldNotExist.json");
     OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson,
-      ValidationRestrictionOptions.builder().allowAdditionalFields(true).build());
+        ValidationRestrictionOptions.builder().allowAdditionalFields(true).build());
   }
 
+  /**
+   * All fields in a schema are expected in the response by design. However, using the
+   * allowableMissingFields you can specify a path to a field that can be missing.
+   */
   @Test
-  public void assertSchema_AllowMissingFields() throws IOException {
+  public void assertSchema_AllowMissingFields_NoValidationIssues() throws IOException {
     URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
     String responseJson = TestResourceHelper.readContentAsString("missingAttribute.json");
     OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson,
-      ValidationRestrictionOptions.builder()
-        .allowAdditionalFields(false)
-        .allowableMissingFields(Set.of("createdDate", "description", "acceptedValues"))
-        .build());
+        ValidationRestrictionOptions.builder()
+            .allowAdditionalFields(false)
+            .allowableMissingFields(Set.of("createdDate", "description", "acceptedValues", "customObject"))
+            .build());
 
     responseJson = TestResourceHelper.readContentAsString("missingRelation.json");
     OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson,
-      ValidationRestrictionOptions.builder()
-        .allowAdditionalFields(true)
-        .allowableMissingFields(Set.of("collectingEvent"))
-        .build());
+        ValidationRestrictionOptions.builder()
+            .allowAdditionalFields(true)
+            .allowableMissingFields(Set.of("collectingEvent"))
+            .build());
   }
 
+  /**
+   * The validator should report a additional relationship field provided that is not specified on
+   * the schema. Validation error is expected here.
+   */
   @Test
-  public void assertSchema_WhenRelationShouldNotExist() throws IOException {
+  public void assertSchema_WhenRelationShouldNotExist_ExpectValidationIssues() throws IOException {
     URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
     String responseJson = TestResourceHelper.readContentAsString("relationShouldNotExist.json");
     Assertions.assertThrows(
-      AssertionFailedError.class,
-      () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson));
+        AssertionFailedError.class,
+        () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", responseJson));
   }
 
-  @Test
-  public void assertRealRemoteSchemaTest() throws IOException {
-    String responseJson = TestResourceHelper.readContentAsString("managedAttributeAPIResponse.json");
-    OpenAPI3Assertions.assertRemoteSchema(
-        new URL("https://raw.githubusercontent.com/DINA-Web/object-store-specs/master/schema/object-store-api.yml"),
-         "ManagedAttribute", responseJson);
-  }
-
+  /**
+   * Test the validators ability to use referenced paths. The customPath.yaml specifies multiple
+   * different paths that go to different yaml files. Validation should still work when these
+   * paths are being used.
+   */
   @Test
   public void assertSchema_WhenReferencedByPath_ValidationEnabled() throws IOException {
     URL specsUrl = this.getClass().getResource("/customPath.yaml");
     Assertions.assertThrows(
-      AssertionFailedError.class,
-      () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute",
-        TestResourceHelper.readContentAsString("missingAttribute.json")));
+        AssertionFailedError.class,
+        () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute",
+            TestResourceHelper.readContentAsString("missingAttribute.json")));
+
     Assertions.assertThrows(
-      AssertionFailedError.class,
-      () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute",
-        TestResourceHelper.readContentAsString("missingRelation.json")));
+        AssertionFailedError.class,
+        () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute",
+            TestResourceHelper.readContentAsString("missingRelation.json")));
+
     OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute",
-      TestResourceHelper.readContentAsString("managedAttributeAPIResponse.json"));
+        TestResourceHelper.readContentAsString("managedAttributeAPIResponse.json"));
   }
 
+  /**
+   * Test with nested objects. This test contains the completed nested object in the response. No
+   * validation issues should occur.
+   */
   @Test
-  public void assertEndPointTest() {
+  public void assertSchema_WhenAdditionalFieldInNestedObjectValid_Successful() throws IOException {
+    // Load in a testing spec.
     URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
-    OpenAPI3Assertions.assertEndpoint(specsUrl, "/v1/managed-attribute", HttpMethod.GET);
+
+    // Test a valid response.
+    String validResponseJson = TestResourceHelper.readContentAsString("additionalFieldInCustomObjectValid.json");
+    Assertions.assertDoesNotThrow(
+        () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", validResponseJson));
   }
 
+  /**
+   * Test with nested objects. This test contains an incomplete nested object in the response.
+   * Validation issues should occur, this will ensure the validator is going through all levels of
+   * the response/schema.
+   */
+  @Test
+  public void assertSchema_WhenAdditionalFieldInNestedObjectValid_ValidationException() throws IOException {
+    // Load in a testing spec.
+    URL specsUrl = this.getClass().getResource("/managedAttribute.yaml");
+
+    // Test an invalid response with contains an additional field in a nest object.
+    String invalidResponseJson = TestResourceHelper.readContentAsString("additionalFieldInCustomObjectInvalid.json");
+    Assertions.assertThrows(AssertionFailedError.class,
+        () -> OpenAPI3Assertions.assertSchema(specsUrl, "ManagedAttribute", invalidResponseJson));
+  }
 }
