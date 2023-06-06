f3150b6eb846c8c5557b2848950ec049f6b45bff
Brandon Andre <andr0284@algonquinlive.com>
2023-06-06 15:49:30 -0400

    Support #31132 - Create SimpleFilterHandlerV2
    - Created SimpleFilterHandlerV2 which copies the existing SimpleFilterHandler but removes all crnk filters with FilterComponents.
    - Updated generateNullComparisonPredicate to work with Ops instead of Crnk operators.

--- dina-base-api/src/main/java/ca/gc/aafc/dina/filter/DinaFilterResolver.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/filter/DinaFilterResolver.java
@@ -239,6 +239,7 @@ public class DinaFilterResolver {
     //Simple Filters
     restrictions.add(SimpleFilterHandler.getRestriction(
       root, cb, rsqlArgumentParser::parse, em.getMetamodel(), querySpec.getFilters()));
+
     //Rsql Filters
     Optional<FilterSpec> rsql = querySpec.findFilter(PathSpec.of("rsql"));
     if (rsql.isPresent() && StringUtils.isNotBlank(rsql.get().getValue())) {
