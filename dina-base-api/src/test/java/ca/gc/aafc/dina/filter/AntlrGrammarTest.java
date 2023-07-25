f16b8d2ee0a259a2ccac69dabb130597e259e0be
Christian Gendreau <christiangendreau@gmail.com>
2023-07-25 11:31:28 -0400

    Improved test Now building a FilterComponent structure

--- dina-base-api/src/test/java/ca/gc/aafc/dina/filter/AntlrGrammarTest.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/filter/AntlrGrammarTest.java
@@ -1,16 +1,24 @@
 package ca.gc.aafc.dina.filter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.jupiter.api.Test;
 
+import com.querydsl.core.types.Ops;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class AntlrGrammarTest {
 
   @Test
-  public void a() {
-    String content = "filter[name][EQ]=jim&filter[position][NEQ]=manager";
+  public void onFilterAsString_structureReturned() {
+    String content = "filter[name][EQ]=jim&filter[position][NEQ]=manager,supervisor";
     ca.gc.aafc.dina.filter.SimpleSearchFilterLexer lexer = new ca.gc.aafc.dina.filter.SimpleSearchFilterLexer(
       CharStreams.fromString(content));
     CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -20,17 +28,51 @@ public class AntlrGrammarTest {
     ParseTree tree = parser.filterExpression();
 
     ParseTreeWalker walker = new ParseTreeWalker();
-    ca.gc.aafc.dina.filter.SimpleSearchFilterBaseListener listener= new MyListener();
+    FilterComponentListener listener= new FilterComponentListener();
     walker.walk(listener, tree);
+
+    FilterComponent fc = listener.buildFilterComponent();
+    assertNotNull(fc);
   }
 
-  public static class MyListener extends ca.gc.aafc.dina.filter.SimpleSearchFilterBaseListener {
+  public static class FilterComponentListener extends ca.gc.aafc.dina.filter.SimpleSearchFilterBaseListener {
+    private final List<FilterComponent> components = new ArrayList<>();
 
     @Override
     public void exitFilterExpression(ca.gc.aafc.dina.filter.SimpleSearchFilterParser.FilterExpressionContext ctx) {
-      System.out.println("attribute:" + ctx.filterAttribute().getText());
-      System.out.println("operator:" + ctx.filterOp().getText());
-      System.out.println("values:" + ctx.filterValue().get(0).getText());
+      // more than 1 value means a OR
+      if (ctx.filterValue().size() > 1) {
+        FilterGroup.FilterGroupBuilder fgBuilder =
+          FilterGroup.builder().conjunction(FilterGroup.Conjunction.OR);
+
+        for (var filterValue : ctx.filterValue()) {
+          fgBuilder.component(new FilterExpression(ctx.filterAttribute().getText(),
+            translateOperator(ctx.filterOp().getText()), filterValue.getText()));
+        }
+        components.add(fgBuilder.build());
+      } else if (ctx.filterValue().size() == 1) {
+        components.add(new FilterExpression(ctx.filterAttribute().getText(),
+          translateOperator(ctx.filterOp().getText()), ctx.filterValue().get(0).getText()));
+      }
+    }
+
+    public FilterComponent buildFilterComponent() {
+      if(components.size() == 1) {
+        return components.get(0);
+      } else if (components.size() > 1) {
+        return
+          FilterGroup.builder().conjunction(FilterGroup.Conjunction.AND)
+            .components(components).build();
+      }
+      return null;
+    }
+
+    public Ops translateOperator(String op) {
+      return switch (op) {
+        case "EQ" -> Ops.EQ;
+        case "NEQ" -> Ops.NE;
+        default -> null;
+      };
     }
 
   }
