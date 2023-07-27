229320c35c65ee10f9dd25e62527bca138113923
Christian Gendreau <cgendreau@users.noreply.github.com>
2023-07-27 15:11:05 -0400

    31659 allow to configure dev user config (#407)
    * 31659 Allow to configure DevUserConfig
    Added config to support group/role of dev-user. Updated documentation.
    
    * Fixed Checkstyle issue

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/config/DevSettings.java
@@ -0,0 +1,40 @@
+package ca.gc.aafc.dina.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import ca.gc.aafc.dina.security.DinaRole;
+
+/**
+ * Settings only for devs in dev mode
+ */
+@Component
+@ConfigurationProperties(prefix = "dev-user")
+@Getter
+@Setter
+public class DevSettings {
+
+  private Map<String, Set<String>> groupRole;
+
+  public Map<String, Set<DinaRole>> getRolesPerGroup() {
+    Map<String, Set<DinaRole>> groupDinaRole = new HashMap<>(groupRole.size());
+
+    for(var entry : groupRole.entrySet()) {
+      groupDinaRole.put(entry.getKey(), entry.getValue().stream()
+        .map(DinaRole::fromString)
+        .map(Optional::orElseThrow).collect(
+        Collectors.toSet()));
+    }
+
+    return groupDinaRole;
+  }
+
+}
