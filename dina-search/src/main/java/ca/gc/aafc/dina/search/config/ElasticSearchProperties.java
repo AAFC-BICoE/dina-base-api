c13257f89bbb83f745bf8e4d6a270f8697b09665
Christian Gendreau <christiangendreau@gmail.com>
2024-01-18 11:33:52 -0500

    32735 Create dina-search module Added new dina-search module to dina-base

---
+++ dina-search/src/main/java/ca/gc/aafc/dina/search/config/ElasticSearchProperties.java
@@ -0,0 +1,26 @@
+package ca.gc.aafc.dina.search.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Loaded from application.yml
+ */
+@ConfigurationProperties(prefix = "elasticsearch")
+@Component
+@Getter
+@Setter
+@Validated
+public class ElasticSearchProperties {
+
+  @NotBlank
+  private String host;
+
+  private int port;
+
+}
