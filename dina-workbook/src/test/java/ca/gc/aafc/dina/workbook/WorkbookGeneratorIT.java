0824b3b81135d65d79876cc426cf7030dd303f93
Christian Gendreau <cgendreau@users.noreply.github.com>
2024-03-04 11:45:35 -0500

    33137 add workbook generator to dina workbook (#438)
    * 33137 Add workbook generator to dina-workbook
    Added WorkbookGenerator + test
    
    * Private constructor, utility class

---
+++ dina-workbook/src/test/java/ca/gc/aafc/dina/workbook/WorkbookGeneratorIT.java
@@ -0,0 +1,31 @@
+package ca.gc.aafc.dina.workbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WorkbookGeneratorIT {
+
+  @Test
+  public void generate_withColumnName_workbookGenerated() throws IOException {
+    Path tmpExport = Files.createTempDirectory("generate_withColumnName_workbookGenerated")
+      .resolve("generatedFile.xlsx");
+    try (Workbook wb = WorkbookGenerator.generate(List.of("col 1", "col 2"))) {
+      wb.write(new FileOutputStream(tmpExport.toFile()));
+    }
+
+    try(FileInputStream fis = new FileInputStream(tmpExport.toFile())) {
+      var result = WorkbookConverter.convertWorkbook(fis);
+      // check value of the first cell of the first row of the first sheet
+      assertEquals("col 1", result.get(0).get(0).content()[0]);
+    }
+  }
+}
