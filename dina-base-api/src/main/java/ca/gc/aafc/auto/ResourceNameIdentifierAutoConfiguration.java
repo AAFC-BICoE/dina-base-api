1b55f150773b3a89e699178f0f1f0a3ec217ad12
Christian Gendreau <christiangendreau@gmail.com>
2024-04-11 09:52:06 -0400

    Try to fix auto-configuration classes

--- dina-base-api/src/main/java/ca/gc/aafc/auto/ResourceNameIdentifierAutoConfiguration.java
+++ dina-base-api/src/main/java/ca/gc/aafc/auto/ResourceNameIdentifierAutoConfiguration.java
@@ -1,9 +1,10 @@
-package ca.gc.aafc.dina.config;
+package ca.gc.aafc.auto;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import ca.gc.aafc.dina.config.ResourceNameIdentifierConfig;
 import ca.gc.aafc.dina.jpa.BaseDAO;
 import ca.gc.aafc.dina.service.ResourceNameIdentifierService;
 
