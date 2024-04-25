5198e184ea60923b54d78930274379a121dd62b8
Christian Gendreau <cgendreau@users.noreply.github.com>
2024-04-25 13:51:44 -0400

    33583 Make RabbitMQMessageProducer more generic (#453)
    Removed generic and change method to accept interface DinaMessage

--- dina-messaging/src/test/java/ca/gc/aafc/dina/messaging/producer/RabbitMQTestProducerQueue2.java
+++ dina-messaging/src/test/java/ca/gc/aafc/dina/messaging/producer/RabbitMQTestProducerQueue2.java
@@ -3,12 +3,10 @@ package ca.gc.aafc.dina.messaging.producer;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
-import ca.gc.aafc.dina.messaging.DinaTestMessage;
-import ca.gc.aafc.dina.messaging.config.TestQueueProperties;
 import ca.gc.aafc.dina.messaging.config.TestSecondQueueProperties;
 
 @Component
-public class RabbitMQTestProducerQueue2 extends RabbitMQMessageProducer<DinaTestMessage>{
+public class RabbitMQTestProducerQueue2 extends RabbitMQMessageProducer {
   public RabbitMQTestProducerQueue2(RabbitTemplate rabbitTemplate,
                                     TestSecondQueueProperties queueProperties) {
     super(rabbitTemplate, queueProperties);
