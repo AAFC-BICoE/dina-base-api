807c2fdc8c588a1e2b211248f42cb74a70a5923b
Christian Gendreau <christiangendreau@gmail.com>
2024-04-25 08:14:33 -0400

    33583 Make RabbitMQMessageProducer more generic Removed generic and change method to accept interface DinaMessage

--- dina-messaging/src/main/java/ca/gc/aafc/dina/messaging/producer/RabbitMQMessageProducer.java
+++ dina-messaging/src/main/java/ca/gc/aafc/dina/messaging/producer/RabbitMQMessageProducer.java
@@ -3,14 +3,12 @@ package ca.gc.aafc.dina.messaging.producer;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import ca.gc.aafc.dina.messaging.DinaMessage;
-
 import ca.gc.aafc.dina.messaging.config.RabbitMQQueueProperties;
 
 /**
- * Generic class to send DineMessage using default routing key (queue name).
- * @param <M>
+ * Generic class to send DinaMessage using default routing key (queue name).
  */
-public class RabbitMQMessageProducer<M extends DinaMessage> {
+public class RabbitMQMessageProducer {
 
   private final RabbitTemplate rabbitTemplate;
   private final String queueName;
@@ -21,7 +19,7 @@ public class RabbitMQMessageProducer<M extends DinaMessage> {
     this.queueName = queueProperties.getQueue();
   }
 
-  public void send(M dinaMessage) {
+  public void send(DinaMessage dinaMessage) {
     // use the queue name as routing key since we are using the default
     rabbitTemplate.convertAndSend(queueName, dinaMessage);
   }
