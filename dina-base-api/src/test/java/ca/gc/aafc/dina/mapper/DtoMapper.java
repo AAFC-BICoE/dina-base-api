0d583d71b951d4121426ed57a362e7b49ed95ac8
Christian Gendreau <christiangendreau@gmail.com>
2024-05-28 11:43:36 -0400

    Combine Context with SourcePropertyName

--- dina-base-api/src/test/java/ca/gc/aafc/dina/mapper/DtoMapper.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/mapper/DtoMapper.java
@@ -1,31 +1,38 @@
 package ca.gc.aafc.dina.mapper;
 
 
-import java.time.OffsetDateTime;
-import java.util.Map;
-import java.util.UUID;
-
+import org.mapstruct.Condition;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.SourcePropertyName;
 import org.mapstruct.factory.Mappers;
 
 import ca.gc.aafc.dina.dto.ChainDto;
+import ca.gc.aafc.dina.dto.ExternalRelationDto;
+import ca.gc.aafc.dina.entity.Chain;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Mapper
 public interface DtoMapper {
 
   DtoMapper INSTANCE = Mappers.getMapper( DtoMapper.class );
 
-  @Mapping(target = "chainTemplate", ignore = true)
-  @Mapping(target = "agent", ignore = true)
-  ChainDto toChainDto(Map<String, String> map);
+  Chain toChain(ChainDto dto, @Context Set<String> provided);
 
-  default UUID objToUUID(Object uuid) {
-    return UUID.fromString(uuid.toString());
+  default UUID externalRelationToUUID(ExternalRelationDto er) {
+    if (er == null) {
+      return null;
+    }
+    return UUID.fromString(er.getId());
   }
 
-  default OffsetDateTime strToOffsetDateTime(String str) {
-    return OffsetDateTime.parse(str);
+  @Condition
+  default boolean isProvided(
+    @SourcePropertyName String sourcePropertyName,
+    @Context Set<String> provided) {
+    return provided.contains(sourcePropertyName);
   }
 
 }
--- dina-base-api/src/test/java/ca/gc/aafc/dina/mapper/DtoMapperTest.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/mapper/DtoMapperTest.java
@@ -1,20 +1,30 @@
 package ca.gc.aafc.dina.mapper;
 
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
 
 import ca.gc.aafc.dina.dto.ChainDto;
+import ca.gc.aafc.dina.entity.Chain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.Set;
+import java.util.UUID;
 
 
 public class DtoMapperTest {
 
   @Test
-  public void a() {
-    ChainDto chain = DtoMapper.INSTANCE.toChainDto(Map.of("group", "grp1"));
-    assertEquals("grp1", chain.getGroup());
+  public void mapWithPartialSet() {
+
+    ChainDto dto = new ChainDto();
+    dto.setGroup("my group");
+    dto.setName("my name");
+    dto.setUuid(UUID.randomUUID());
+    // set only the properties provided
+    Chain chain = DtoMapper.INSTANCE.toChain(dto, Set.of("group"));
+
+    assertEquals("my group", chain.getGroup());
+    assertNull(chain.getName());
   }
 }
