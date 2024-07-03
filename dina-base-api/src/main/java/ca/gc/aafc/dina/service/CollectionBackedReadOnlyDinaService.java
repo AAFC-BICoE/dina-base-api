61d79df2c9a58f29dafa39855ebf65fc5912c4c5
Christian Gendreau <christiangendreau@gmail.com>
2024-07-03 11:35:51 -0400

    34160 Add CollectionBackedReadOnlyService base class Added new class + updated test

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/service/CollectionBackedReadOnlyDinaService.java
@@ -0,0 +1,46 @@
+package ca.gc.aafc.dina.service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Implementation of {@link PredicateBasedReadOnlyDinaService} that is backed by a Java collection.
+ *
+ * @param <K>
+ * @param <R>
+ */
+public abstract class CollectionBackedReadOnlyDinaService<K, R> implements PredicateBasedReadOnlyDinaService<K, R> {
+
+  private final Collection<R> collection;
+  private final Function<R, K> getKeyFunction;
+
+  public CollectionBackedReadOnlyDinaService(Collection<R> collection,
+                                             Function<R, K> getKeyFunction) {
+    this.collection = collection;
+    this.getKeyFunction = getKeyFunction;
+  }
+
+  @Override
+  public R findOne(K key) {
+    return collection.stream()
+      .filter(r -> getKeyFunction.apply(r).equals(key)).findFirst()
+      .orElse(null);
+  }
+
+  @Override
+  public List<R> findAll(Predicate<R> predicate, Integer pageOffset, Integer pageLimit) {
+    Stream<R> stream = collection.stream().filter(predicate);
+
+    if (pageOffset != null) {
+      stream = stream.skip(pageOffset);
+    }
+    if (pageLimit != null) {
+      stream = stream.limit(pageLimit);
+    }
+    return stream.collect(Collectors.toList());
+  }
+}
