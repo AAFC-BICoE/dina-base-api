90d355a72d1b42a5e56d6528f24de14d3907f793
Christian Gendreau <christiangendreau@gmail.com>
2025-01-13 10:46:55 -0500

    Exception handler ConstraintViolationException

--- dina-base-api/src/main/java/ca/gc/aafc/dina/exceptionmapping/JsonApiExceptionControllerAdvice.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/exceptionmapping/JsonApiExceptionControllerAdvice.java
@@ -1,5 +1,7 @@
 package ca.gc.aafc.dina.exceptionmapping;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
 
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.toedter.spring.hateoas.jsonapi.JsonApiError;
+import com.toedter.spring.hateoas.jsonapi.JsonApiErrors;
 
 import ca.gc.aafc.dina.repository.DinaRepositoryV2;
 
@@ -18,13 +21,40 @@ import ca.gc.aafc.dina.repository.DinaRepositoryV2;
 public class JsonApiExceptionControllerAdvice {
 
   @ExceptionHandler
-  public ResponseEntity<JsonApiError> handleValidationException(ValidationException ex) {
+  public ResponseEntity<JsonApiErrors> handleValidationException(ValidationException ex) {
     return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(
-      JsonApiError.create()
-        .withCode(HttpStatus.UNPROCESSABLE_ENTITY.toString())
-        .withTitle("Validation error")
-        .withDetail(ex.getMessage())
-        .withStatus(HttpStatus.UNPROCESSABLE_ENTITY.toString())
+      JsonApiErrors.create().withError(
+        JsonApiError.create()
+          .withCode(Integer.toString(HttpStatus.UNPROCESSABLE_ENTITY.value()))
+          .withStatus(HttpStatus.UNPROCESSABLE_ENTITY.toString())
+          .withTitle("Validation error")
+          .withDetail(ex.getMessage()))
     );
+  }
+
+  @ExceptionHandler
+  public ResponseEntity<JsonApiErrors> handleConstraintViolationException(ConstraintViolationException ex) {
+    JsonApiErrors errors = JsonApiErrors.create();
+    ex.getConstraintViolations()
+      .stream()
+      .map(cv -> JsonApiError.create()
+        .withCode(Integer.toString(HttpStatus.UNPROCESSABLE_ENTITY.value()))
+        .withStatus(HttpStatus.UNPROCESSABLE_ENTITY.toString())
+        .withTitle("Constraint violation")
+        .withDetail(generateDetail(cv))
+        .withSourcePointer(generateSourcePointer(cv))
+      )
+      .forEach(errors::withError);
+
+    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(
+      errors);
+  }
+
+  private static String generateDetail(ConstraintViolation<?> cv) {
+    return String.join(" ", cv.getPropertyPath().toString(), cv.getMessage());
+  }
+
+  private static String generateSourcePointer(ConstraintViolation<?> cv) {
+    return cv.getPropertyPath().toString();
   }
 }
