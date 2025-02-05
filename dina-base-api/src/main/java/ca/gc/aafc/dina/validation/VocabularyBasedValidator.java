8bfed0dd22097664e5d630e7d1cb483a17bd3ceb
John Phan <johnphan010@gmail.com>
2025-02-05 10:05:33 -0800

    35663 Refactor VocabularyBased Validator to use bounded wildcard type (#485)
    - Refactored functions to use <? extends VocabularyElementConfiguration> where necessary

--- dina-base-api/src/main/java/ca/gc/aafc/dina/validation/VocabularyBasedValidator.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/validation/VocabularyBasedValidator.java
@@ -9,25 +9,29 @@ import org.springframework.validation.Errors;
 
 import ca.gc.aafc.dina.vocabulary.VocabularyElementConfiguration;
 
-public abstract class VocabularyBasedValidator <T> extends DinaBaseValidator<T> {
+public abstract class VocabularyBasedValidator<T> extends DinaBaseValidator<T> {
 
   public static final String KEY_NOT_FOUND = "vocabulary.keyNotFound";
 
-  public VocabularyBasedValidator(Class<T> supportedClass, @Named("validationMessageSource") MessageSource messageSource) {
+  public VocabularyBasedValidator(Class<T> supportedClass,
+      @Named("validationMessageSource") MessageSource messageSource) {
     super(supportedClass, messageSource);
   }
 
   /**
-   * Checks if the provided value matches an entry in vocabularyElement list by comparing it to keys (ignore case).
-   * @param value value to validate and standardize
-   * @param fieldName used to report error
+   * Checks if the provided value matches an entry in vocabularyElement list by
+   * comparing it to keys (ignore case).
+   * 
+   * @param value              value to validate and standardize
+   * @param fieldName          used to report error
    * @param vocabularyElements valid elements for the vocabulary
-   * @param errors stores the errors
-   * @return standardized value of the provided value or the same value if an error occurred
+   * @param errors             stores the errors
+   * @return standardized value of the provided value or the same value if an
+   *         error occurred
    */
-  protected String validateAndStandardizeValueAgainstVocabulary(String value, String fieldName, List<VocabularyElementConfiguration> vocabularyElements, Errors errors) {
-    Optional<VocabularyElementConfiguration>
-      foundVocabularyElement = findInVocabulary(value, vocabularyElements);
+  protected String validateAndStandardizeValueAgainstVocabulary(String value, String fieldName,
+      List<? extends VocabularyElementConfiguration> vocabularyElements, Errors errors) {
+    Optional<? extends VocabularyElementConfiguration> foundVocabularyElement = findInVocabulary(value, vocabularyElements);
     if (foundVocabularyElement.isPresent()) {
       return foundVocabularyElement.get().getKey();
     } else {
@@ -38,16 +42,18 @@ public abstract class VocabularyBasedValidator <T> extends DinaBaseValidator<T> 
   }
 
   /**
-   * Checks that each value are in the vocabulary (matching the key, case-sensitive)
+   * Checks that each value are in the vocabulary (matching the key,
+   * case-sensitive)
    * Only one error will be reported even if multiple values are not matching.
+   * 
    * @param values
    * @param fieldName
    * @param vocabularyElements
    * @param errors
    */
   protected void validateValuesAgainstVocabulary(List<String> values, String fieldName,
-                                                 List<VocabularyElementConfiguration> vocabularyElements,
-                                                 Errors errors) {
+      List<? extends VocabularyElementConfiguration> vocabularyElements,
+      Errors errors) {
     for (String value : values) {
       if (!isInVocabulary(value, vocabularyElements)) {
         String errorMessage = getMessage(KEY_NOT_FOUND, value, fieldName);
@@ -59,11 +65,12 @@ public abstract class VocabularyBasedValidator <T> extends DinaBaseValidator<T> 
 
   /**
    * Checks if the provided key (case-sensitive) is in the vocabulary.
-   * @param key key to check
+   * 
+   * @param key                key to check
    * @param vocabularyElements
    * @return key is in the vocabulary or not
    */
-  protected boolean isInVocabulary(String key, List<VocabularyElementConfiguration> vocabularyElements) {
+  protected boolean isInVocabulary(String key, List<? extends VocabularyElementConfiguration> vocabularyElements) {
     for (VocabularyElementConfiguration el : vocabularyElements) {
       if (el.getKey().equals(key)) {
         return true;
@@ -73,12 +80,15 @@ public abstract class VocabularyBasedValidator <T> extends DinaBaseValidator<T> 
   }
 
   /**
-   * Finds the first CollectionVocabularyElement where the key is matching (ignore case) the provided value.
+   * Finds the first CollectionVocabularyElement where the key is matching (ignore
+   * case) the provided value.
+   * 
    * @param key
    * @param vocabularyElements
    * @return
    */
-  protected Optional<VocabularyElementConfiguration> findInVocabulary(String key, List<VocabularyElementConfiguration> vocabularyElements) {
+  protected Optional<? extends VocabularyElementConfiguration> findInVocabulary(String key,
+      List<? extends VocabularyElementConfiguration> vocabularyElements) {
     return vocabularyElements.stream().filter(o -> o.getKey().equalsIgnoreCase(key)).findFirst();
   }
 }
