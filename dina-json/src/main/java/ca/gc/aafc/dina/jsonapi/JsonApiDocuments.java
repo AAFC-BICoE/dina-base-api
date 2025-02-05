44f4520adac05b3f97493143b07775815e83756f
Christian Gendreau <christiangendreau@gmail.com>
2025-02-05 14:17:53 -0500

    Added JsonApiDocuments with relationships

--- dina-json/src/main/java/ca/gc/aafc/dina/jsonapi/JsonApiDocuments.java
+++ dina-json/src/main/java/ca/gc/aafc/dina/jsonapi/JsonApiDocuments.java
@@ -1,6 +1,8 @@
 package ca.gc.aafc.dina.jsonapi;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -21,6 +23,9 @@ public final class JsonApiDocuments {
    */
   public static JsonApiDocument createJsonApiDocument(UUID uuid, String type,
                                                       Map<String, Object> attributes) {
+    Objects.requireNonNull(uuid);
+    Objects.requireNonNull(type);
+    
     return JsonApiDocument.builder()
       .data(
         JsonApiDocument.ResourceObject.builder()
@@ -29,5 +34,40 @@ public final class JsonApiDocuments {
           .attributes(attributes)
           .build()
       ).build();
+  }
+
+  /**
+   * Creates a {@link JsonApiDocument} with relationships for provided parameters
+   * @param uuid
+   * @param type
+   * @param attributes
+   * @param relationships
+   * @return
+   */
+  public static JsonApiDocument createJsonApiDocument(UUID uuid, String type,
+                                                      Map<String, Object> attributes,
+                                                      Map<String, Object> relationships) {
+    Objects.requireNonNull(uuid);
+    Objects.requireNonNull(type);
+    Objects.requireNonNull(relationships);
+
+    var resourceObjectBuilder = JsonApiDocument.ResourceObject.builder()
+      .id(uuid)
+      .type(type)
+      .attributes(attributes);
+
+    if (!relationships.isEmpty()) {
+      Map<String, JsonApiDocument.RelationshipObject> relationshipObjects =
+        new HashMap<>(relationships.size());
+      for (var rel : relationships.entrySet()) {
+        relationshipObjects.put(rel.getKey(),
+          JsonApiDocument.RelationshipObject.builder().data(rel.getValue()).build());
+      }
+      resourceObjectBuilder.relationships(relationshipObjects);
+    }
+
+    return JsonApiDocument.builder()
+      .data(resourceObjectBuilder.build())
+      .build();
   }
 }
