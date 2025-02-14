5c97a35854ac84727713a724fdab17ad8ef45b82
Christian Gendreau <christiangendreau@gmail.com>
2025-02-14 09:59:08 -0500

    Added tests

--- dina-base-api/src/test/java/ca/gc/aafc/dina/security/DinaRoleTest.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/security/DinaRoleTest.java
@@ -2,9 +2,14 @@ package ca.gc.aafc.dina.security;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DinaRoleTest {
 
@@ -17,6 +22,22 @@ public class DinaRoleTest {
     assertEquals(Optional.empty(), DinaRole.fromString(" dina-admin"));
 
     assertEquals(Optional.empty(), DinaRole.fromString(null));
+  }
+
+  @Test
+  public void testAdminAndGroupBased() {
+
+    assertTrue(DinaRole.adminBasedRoles().contains(DinaRole.DINA_ADMIN));
+    assertFalse(DinaRole.adminBasedRoles().contains(DinaRole.SUPER_USER));
+
+    assertTrue(DinaRole.groupBasedRoles().contains(DinaRole.SUPER_USER));
+    assertFalse(DinaRole.groupBasedRoles().contains(DinaRole.DINA_ADMIN));
+
+    //make sure all groups are covered
+    List<DinaRole> allRoles = Arrays.stream(DinaRole.values()).collect(Collectors.toList());
+    allRoles.removeAll(DinaRole.adminBasedRoles());
+    allRoles.removeAll(DinaRole.groupBasedRoles());
+    assertEquals(0, allRoles.size());
   }
 
 }
