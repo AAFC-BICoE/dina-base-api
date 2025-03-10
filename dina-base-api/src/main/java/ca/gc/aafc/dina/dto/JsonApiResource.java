75834b2a7b8d32b5460a90285ddce57f7a9c3ff6
Brandon Andre <brandon.andre@agr.gc.ca>
2025-03-10 10:46:38 -0400

    Support #35862 - Update comments for JsonApiResource (#490)
    - Updated the comment

--- dina-base-api/src/main/java/ca/gc/aafc/dina/dto/JsonApiResource.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/dto/JsonApiResource.java
@@ -5,6 +5,18 @@ import java.util.UUID;
 /**
  * Interface of a resource (usually DTO) that can be exposed in JSON:API.
  *
+ * Required for resources being used in relationships to retrieve the type and uuid.
+ * 
+ * For example, when a resource has a relationship to another resource:
+ * 
+ * <pre>
+ * <code>
+ * &#064;JsonApiRelation
+ * private List&lt;OrganizationDto&gt; organizations;
+ * </code>
+ * </pre>
+ * 
+ * The related resource (e.g. OrganizationDto) will need to implement JsonApiResource.
  */
 public interface JsonApiResource {
   String getJsonApiType();
