c86609e4bbbcdeef00d095075b9a730a28e55811
Christian Gendreau <christiangendreau@gmail.com>
2025-03-13 10:58:52 -0400

    Added missing file

---
+++ dina-json/src/main/java/ca/gc/aafc/dina/jsonapi/JsonApiBulkResourceIdentifierDocument.java
@@ -0,0 +1,21 @@
+package ca.gc.aafc.dina.jsonapi;
+
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+
+/**
+ * Represents a bulk request with only resource identifier (GET and DELETE).
+ *
+ */
+@Jacksonized
+@SuperBuilder
+@Getter
+public class JsonApiBulkResourceIdentifierDocument {
+
+  private List<JsonApiDocument.ResourceIdentifier> data;
+  private Map<String, Object> meta;
+
+}
