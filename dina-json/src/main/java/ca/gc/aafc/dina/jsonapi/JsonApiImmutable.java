2fe55c90cb59642c4a5075f16993200ee55d5905
Christian Gendreau <christiangendreau@gmail.com>
2025-07-10 14:27:37 -0400

    Fixed Checkstyle

--- dina-json/src/main/java/ca/gc/aafc/dina/jsonapi/JsonApiImmutable.java
+++ dina-json/src/main/java/ca/gc/aafc/dina/jsonapi/JsonApiImmutable.java
@@ -8,11 +8,11 @@ import java.lang.annotation.Target;
 /**
  * Identify immutability of field on a DTO to control some values that can not be created or updated.
  */
-@Target({ElementType.FIELD})
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonApiImmutable {
 
-  enum ImmutableOn {CREATE, UPDATE}
+  enum ImmutableOn { CREATE, UPDATE }
 
   ImmutableOn value();
 }
