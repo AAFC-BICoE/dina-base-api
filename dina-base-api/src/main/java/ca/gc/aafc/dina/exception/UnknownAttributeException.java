4f8627f6b18aeb0781bec5ab33d6663221ee6dfa
Christian Gendreau <christiangendreau@gmail.com>
2025-07-11 09:55:07 -0400

    Prepare 0.147 release

--- dina-base-api/src/main/java/ca/gc/aafc/dina/exception/UnknownAttributeException.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/exception/UnknownAttributeException.java
@@ -4,11 +4,12 @@ package ca.gc.aafc.dina.exception;
  * DINA specific {@link RuntimeException} that indicates that an attribute can not be found on a specific object.
  */
 public class UnknownAttributeException extends RuntimeException {
+
   public UnknownAttributeException(Throwable cause) {
     super(cause);
   }
 
-  public UnknownAttributeException(String message) {
-    super(message);
+  public UnknownAttributeException(String attributeName) {
+    super(attributeName + " : unknown attribute");
   }
 }
