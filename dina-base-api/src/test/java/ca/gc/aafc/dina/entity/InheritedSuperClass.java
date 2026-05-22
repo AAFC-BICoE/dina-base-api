47eb2dad8a3161b40dc70460113f3ec26d86b16b
Christian Gendreau <cgendreau@users.noreply.github.com>
2026-05-22 15:45:51 -0400

    38317 Change javax.persistence.loadgraph hint (#571)
    Changed hint and removed legacy dependencies that could cause old includes to compile

--- dina-base-api/src/test/java/ca/gc/aafc/dina/entity/InheritedSuperClass.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/entity/InheritedSuperClass.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.MappedSuperclass;
+import jakarta.persistence.MappedSuperclass;
 
 @Getter
 @Setter
--- dina-base-api/src/test/java/ca/gc/aafc/dina/entity/parentchild/Parent.java
+++ dina-base-api/src/test/java/ca/gc/aafc/dina/entity/parentchild/Parent.java
@@ -7,12 +7,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
