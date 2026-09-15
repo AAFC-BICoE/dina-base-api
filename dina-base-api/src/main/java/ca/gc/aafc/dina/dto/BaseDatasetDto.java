10a732a8fa37a6f57da9bc389310f1542e720dde
Christian Gendreau <cgendreau@users.noreply.github.com>
2026-09-15 08:40:21 -0400

    Augment BaseDatasetDto (#597)

--- dina-base-api/src/main/java/ca/gc/aafc/dina/dto/BaseDatasetDto.java
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/dto/BaseDatasetDto.java
@@ -25,8 +25,14 @@ public class BaseDatasetDto implements Serializable {
 
   public static final String AGENT_ROLE_CREATOR = "creator";
   public static final String AGENT_ROLE_METADATA_PROVIDER = "metadataProvider";
+  public static final String AGENT_ROLE_CONTACT = "contact";
+  public static final String AGENT_ROLE_ASSOCIATED_PARTY = "associatedParty";
+  public static final String AGENT_ROLE_PUBLISHER = "publisher";
 
   protected UUID uuid;
+  // store as string in case we need semantic versioning
+  protected String datasetVersion;
+  protected LocalDate publicationDate;
   protected String group;
 
   protected MultilingualTitle multilingualTitle;
@@ -38,6 +44,9 @@ public class BaseDatasetDto implements Serializable {
 
   protected List<KeywordSet> keywordSets = List.of();
   protected Coverage coverage;
+
+  protected Methods methods;
+  protected Project project;
 
   /**
    * Information describing the licence and conditions governing the use and
@@ -96,5 +105,49 @@ public class BaseDatasetDto implements Serializable {
     String scientificName,
     String commonName) implements Serializable {
   }
-  
+
+  /**
+   * Scientific methods used to collect or produce the dataset.
+   */
+  @Builder
+  public record Methods(
+    List<String> methodSteps,
+    Sampling sampling,
+    List<String> qualityControlDescriptions) implements Serializable {
+  }
+
+  /**
+   * Sampling procedures and study extent.
+   */
+  @Builder
+  public record Sampling(
+    String studyExtent,
+    String samplingDescription) implements Serializable {
+  }
+
+  /**
+   * Information describing the project associated with the dataset.
+   */
+  @Builder
+  public record Project(
+    String title,
+    String abstractText,
+    String funding,
+    List<AgentRoles> personnel,
+    List<Award> awards,
+    String studyAreaDescription,
+    String designDescription) implements Serializable {
+  }
+
+  /**
+   * Structured funding award information.
+   */
+  @Builder
+  public record Award(
+    String funderName,
+    List<String> funderIdentifiers,
+    String awardNumber,
+    String title,
+    String awardUrl) implements Serializable {
+  }
 }
