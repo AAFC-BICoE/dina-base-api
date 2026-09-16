3929908f0083724c013491ee568a702c3d3ead64
Christian Gendreau <christiangendreau@gmail.com>
2026-09-16 15:33:06 -0400

    81539 Add DuplicateException and mapper Added new exception and json api mapper

---
+++ dina-base-api/src/main/java/ca/gc/aafc/dina/exception/DuplicateResourceException.java
@@ -0,0 +1,26 @@
+package ca.gc.aafc.dina.exception;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import lombok.Getter;
+
+@Getter
+public class DuplicateResourceException extends RuntimeException {
+  private final String resourceType;
+  private final String sourcePointer;
+
+  public static DuplicateResourceException create(String resourceType, UUID duplicateResourceId, String sourcePointer) {
+    return new DuplicateResourceException(resourceType, Objects.toString(duplicateResourceId), sourcePointer);
+  }
+
+  public static DuplicateResourceException create(String resourceType, String duplicateResourceId, String sourcePointer) {
+    return new DuplicateResourceException(resourceType, duplicateResourceId, sourcePointer);
+  }
+
+  private DuplicateResourceException(String resourceType, String duplicateResourceId, String sourcePointer) {
+    super("Duplicate " + resourceType + " detected. Existing resource ID: " + duplicateResourceId);
+    this.resourceType = resourceType;
+    this.sourcePointer = sourcePointer;
+  }
+}
